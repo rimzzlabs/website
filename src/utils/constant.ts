bc584577826a966efd5fcc6f4181e80f6320c7f9
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-16 22:02:42 +0700

    feat(src/utils): adding some jsDoc, and modifying metaPages

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,7 +4,11 @@ import {
   IoLogoLinkedin,
   IoLogoFacebook
 } from 'react-icons/io5'
+import { TechincalType } from '@/types/customType'
 
+/**
+ * @description Routes, defines the routes of the pages, if you add a new route the page would also updated
+ */
 export const routes = [
   {
     path: '/',
@@ -16,6 +20,10 @@ export const routes = [
   }
 ]
 
+/**
+ * @description links mean your social media, and this would be used in the `Footer` component, look up to the `src/components/Footer.tsx`.
+ * there you would fine this array being used to populate the data inside the `Footer` component.
+ */
 export const links = [
   {
     name: 'email',
@@ -39,12 +47,16 @@ export const links = [
   }
 ]
 
+/**
+ * @description an object of object, this is the meta data of each page, currently just for mockup purpose.
+ * later on I will not prefer using this as it is not scalable and very confusing
+ */
 export const metaPages = {
   home: {
     title: 'Home',
-    description: `I am a Student of Informatics Management at AMIK Serang and I'm a Frontend Web Developer who love to work with.`,
+    description: `sI am a 18 Y.O flexible guy who loves to learn new things, I like to read article about science and technology. I couldn't help with the science tho, because I have no idea how it works, but I still love to read and learn a bit about it.`,
     image:
-      'https://ik.imagekit.io/mlnzyx/screenshot-site_Tj2pg8xYTe4.png?updatedAt=1639472680463',
+      'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
     url: 'https://rizkicitra.my.id'
   },
   article: {
@@ -52,32 +64,61 @@ export const metaPages = {
     description:
       'In this page you can see my article, I often to write about my daily life and about my hobbies.',
     image:
-      'https://ik.imagekit.io/mlnzyx/screenshot-site_Tj2pg8xYTe4.png?updatedAt=1639472680463',
+      'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
     url: 'https://rizkicitra.my.id/article'
   }
 }
 
-// often activites
+/**
+ * @description This is a constant for the type of Technical Skills, all those skills are stored here,
+ * modify this to match with your own skills, or you can add more items in the array, please don't foget to check the type of the items
+ * in the `src/types/customType.ts`
+ */
+export const TechincalSkill: Array<TechincalType> = [
+  {
+    title: 'Markup and Stylesheet',
+    Icon: ['markdown', 'html', 'css', 'sass', 'tailwindcss'],
+    description: [
+      `I have 1 Year experience with HTML and CSS, I have also learn SASS to improve my writing stylesheet, but my main tools of using Stylesheet is TailwindCSS.`,
+      'Markdown is a simple way to write text that can be converted to HTML, and everyone know how to use it.'
+    ]
+  },
+  {
+    title: 'JavaScript and TypeScript',
+    Icon: ['js', 'ts'],
+    description: `I have 1 Year experience with JavaScript, and about 3 Months with TypeScript, TypeScript is very heplful when it comes to writing code, it static type feature are awesome, and I am currently using it`
+  },
+  {
+    title: 'React and Next.js',
+    Icon: ['react', 'nextjs'],
+    description: `I have 10 Months experience with React, and about 6 Months with Next.js, Next.js is a framework that is very easy to use, and I am currently using it as my main go to frontend framework.`
+  },
+  {
+    title: 'Backend Environment',
+    Icon: ['nodejs', 'express', 'strapi', 'php'],
+    description: `Although this should be not here, I've recently learning and doing some backend development, even though it's on localhost, I'm made my first API with express and I'm still learning to become a Fullstack JavaScript.`
+  }
+]
 
-export const spareTime = [
+/**
+ * @description list of hobies, you can add your own or even modify this, such as adding description or whatever you want
+ * But don't forget to check HobbiesCard component to match with your customization on `src/component/cards` folder
+ */
+export const hobies = [
   {
-    title: 'Watch Anime',
-    description:
-      'I just love to watch anime instead of a news about my country, Anime is the only thing that I can watch, I enjoy watching it, because it is only a show that nice to watch and enjoyable to watch.'
+    title: 'Watch Movies or Animes',
+    icon: 'tv'
   },
   {
-    title: 'Read article about IT',
-    description:
-      'As a Developer, Read devpost daily is something that I should do in my opinion, everyday people share about they thought, and it is very interested to read and give me a new insight about Information Technology'
+    title: 'Read Techincal Article',
+    icon: 'book'
   },
   {
-    title: 'Drink Coffee',
-    description:
-      'this is very important because my only precious fuel is only a coffee (JK lol).'
+    title: 'Try something new',
+    icon: 'code'
   },
   {
-    title: 'Play Game',
-    description:
-      "I'm not that guy who loves to playing game for a long time, but sometimes it' s fun "
+    title: 'Playing Games',
+    icon: 'gamepad'
   }
 ]
