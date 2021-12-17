2e98a3a794bcde50ae58e2b02a20bbb3335f34ad
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-17 14:28:35 +0700

    feat(src/hooks/): add JSDoc

--- src/hooks/useSmoothScroll.tsx
+++ src/hooks/useSmoothScroll.tsx
@@ -1,19 +1,18 @@
+import { RefObject } from 'react'
+
 /**
- * smooth scrol on click!
+ * @description smooth scrol on click!
  * this custom hooks used to scoll trough targetted element smoothly
  * the function accept one parameters
  * ref: refered element that would be targetted
- * ref has value like this
- * {
- *  current: element
- * }
- * so to scroll through that element, you should acces it's <current> key inside of the object
+ * @example
+ * // ref has value like this
+ * ref = {current: element }
+ * @description
+ * so to scroll through that element, you should acces it's `current` key inside of the object
  * if the ref valid, then return a function to scroll throught that element,
  * the function returned act like an eventListener
  */
-
-import { RefObject } from 'react'
-
 const useSmoothScroll = (ref: RefObject<HTMLDivElement>) => {
   if (!ref) throw new Error('a reference of element should be provided')
   const scrollToRef = () => {
