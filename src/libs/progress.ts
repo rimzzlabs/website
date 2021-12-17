3e952f6356a020c52fd3761b8b8b01c9fed73ac7
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-17 14:25:41 +0700

    feat: add JSDoc

--- src/libs/progress.ts
+++ src/libs/progress.ts
@@ -1,5 +1,9 @@
 import ProgressBar from '@badrap/bar-of-progress'
 
+/**
+ * @description intialize progressbar from `@badrap/bar-of-progress` and
+ * set configuration of className, delay and size of the progressbar
+ */
 export const progress = new ProgressBar({
   className: 'bg-gradient-to-r from-primary-500 via-indigo-500 to-rose-500',
   delay: 0,
