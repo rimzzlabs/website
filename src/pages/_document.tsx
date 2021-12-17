3b82d86161aae9acba5bc055d4c4d13f16a457aa
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-17 10:16:46 +0700

    feat(src/pages/_document.tsx): adding meta theme color and link rel of apple-touch-icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default class CustomDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta name='theme-color' content='#171717' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
         </Head>
