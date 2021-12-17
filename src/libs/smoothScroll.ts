f9c755ec9990d073df140bef4f36718c14e76f46
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-17 09:51:00 +0700

    style(src/libs): removing unused modules

--- src/libs/smoothScroll.ts
+++
@@ -1,8 +0,0 @@
-// parameter e is an even onClick and scroll to some element with id
-export const smoothScroll = () => {
-  const html: HTMLElement = document.documentElement
-  html.style.scrollBehavior = 'smooth'
-  setTimeout(() => {
-    html.style.scrollBehavior = 'unset'
-  }, 0)
-}
