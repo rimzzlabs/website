f8bad50d00c9a4d226791f2c917b66e99e5988ea
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-21 15:52:42 +0700

    feat(src/pages/index.tsx, src/utils/): modifying to has better sentence, and updating constant files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,101 @@
-import clsx from 'clsx'
-import Footer from '@/components/Footer'
-import Meta from '@/components/atoms/Meta'
-import NextLink from '@/components/NextLink'
-import NextImage from '@/components/NextImage'
-import HobbyCard from '@/components/cards/HobbyCard'
-import FullPage from '@/components/wrapper/FullPage'
-import useSmoothScroll from '@/hooks/useSmoothScroll'
-import SkillCard from '@/components/cards/SkillCard'
-import ProjectCard from '@/components/cards/ProjectCard'
-import AnimeContainer from '@/components/wrapper/AnimeContainer'
-import { hobies, metaPages, TechincalSkill } from '@/utils/constant'
-import { useRef } from 'react'
-import { doGet } from '@/libs/doFetch'
-import { IoLogoGithub } from 'react-icons/io5'
-import type { ProjectType } from '@/types/customType'
-import type { GetStaticProps } from 'next'
+import clsx from 'clsx';
+import Footer from '@/components/Footer';
+import Meta from '@/components/atoms/Meta';
+import NextLink from '@/components/NextLink';
+import NextImage from '@/components/NextImage';
+import HobbyCard from '@/components/cards/HobbyCard';
+import FullPage from '@/components/wrapper/FullPage';
+import useSmoothScroll from '@/hooks/useSmoothScroll';
+import SkillCard from '@/components/cards/SkillCard';
+import ProjectCard from '@/components/cards/ProjectCard';
+import AnimeContainer from '@/components/wrapper/AnimeContainer';
+import { hobies, metaPages, TechincalSkill } from '@/utils/constant';
+import { useRef } from 'react';
+import { doGet } from '@/libs/doFetch';
+import { IoLogoGithub, IoMail } from 'react-icons/io5';
+import type { ProjectType } from '@/types/customType';
+import type { GetStaticProps } from 'next';
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: await doGet('/project?sort=id:DESC'),
     revalidate: 15
-  }
-}
+  };
+};
 
 type IndexPageProps = {
-  result: ProjectType
-}
+  result: ProjectType;
+};
 
 const IndexPage = ({ result }: IndexPageProps) => {
-  const footerRef = useRef<HTMLDivElement>(null)
-  const scrollToRef = useSmoothScroll(footerRef)
+  const footerRef = useRef<HTMLDivElement>(null);
+  const scrollToRef = useSmoothScroll(footerRef);
 
   return (
     <>
-      <FullPage className='flex items-center'>
+      <FullPage className="flex items-center">
         <Meta {...metaPages.home} />
         <section>
-          <p className='font-bold text-2xl md:text-5xl text-typo-700 dark:text-typo-200'>
-            <span className='font-normal select-none animate-pulse'>👋</span>
+          <p className="font-bold text-2xl md:text-5xl text-typo-700 dark:text-typo-200">
+            <span className="font-normal select-none animate-pulse">👋</span>
             Hello, I am
           </p>
-          <h1 className='xl:text-6xl 2xl:text-7xl my-2 md:my-4'>
-            Rizki Maulana Citra
-          </h1>
-          <p className='max-w-3xl md:text-lg 2xl:text-xl'>
-            A Student of Informatics Management at AMIK Serang and I&apos;m a
-            Frontend Web Developer who love to work with JavaScript and{' '}
-            <NextLink href='https://github.com/enaqx/awesome-react' unstyled>
-              React Ecosystem
+          <h1 className="xl:text-6xl 2xl:text-7xl my-2 md:my-4">Rizki Maulana Citra</h1>
+          <p className="max-w-3xl md:text-lg 2xl:text-xl">
+            A student at AMIK Serang and a Frontend Web Developer who enjoys working with JavaScript and the{' '}
+            <NextLink href="https://github.com/enaqx/awesome-react" unstyled>
+              React ecosystem
             </NextLink>
             .
           </p>
-          <p className='max-w-3xl md:text-lg 2xl:text-xl mt-2 md:mt-3'>
-            I&apos;m a passionate about Web Development and I&apos;m always
-            looking for a new challenge, feel free to{' '}
-            <NextLink onClick={scrollToRef} href='#contact' unstyled>
+          <p className="max-w-3xl md:text-lg 2xl:text-xl mt-2 md:mt-3">
+            I&apos;m passionate about Web Development and always looking for new challenges and experiences, please do{' '}
+            <NextLink onClick={scrollToRef} href="#contact" unstyled>
               contact me
             </NextLink>{' '}
-            if you have any thoughts!
+            if you have any questions.
           </p>
         </section>
       </FullPage>
 
-      <div className='py-20 md:py-40'>
-        <AnimeContainer
-          className={clsx(
-            'flex flex-col-reverse w-full',
-            'md:flex-row md:justify-between'
-          )}>
-          <section className='w-full md:pr-4 lg:pr-0'>
-            <h2 className='header-color'>About Me</h2>
-            <p className='max-w-3xl 2xl:text-lg'>
-              My name is Rizki Maulana Citra, I&apos;m from Pandeglang, Banten.
-              Banten is one of the most beautiful city in Indonesia. You&apos;ve
-              probably heard about{' '}
-              <NextLink
-                href='https://en.wikipedia.org/wiki/Ujung_Kulon_National_Park'
-                unstyled>
+      <div className="py-20 md:py-40">
+        <AnimeContainer className={clsx('flex flex-col-reverse w-full', 'md:flex-row md:justify-between')}>
+          <section className="w-full md:pr-4 lg:pr-0">
+            <h2 className="header-color">About Me</h2>
+            <p className="max-w-3xl 2xl:text-lg">
+              My name is Rizki Maulana Citra, I was born in Pandeglang Banten. If you don&apos;t know Banten, you may
+              have heard of{' '}
+              <NextLink href="https://en.wikipedia.org/wiki/Ujung_Kulon_National_Park" unstyled>
                 Ujung Kulon National Park
               </NextLink>
               .
             </p>
-            <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-              I am a 18 Y.O flexible guy who loves to learn new things, I like
-              to read article about science and technology. I couldn&apos;t help
-              with the science tho, because I have no idea how it works, but I
-              still love to read and learn a bit about it.
+            <p className="max-w-3xl 2xl:text-lg mt-2 md:mt-4">
+              I am an 18 year old who is passionate about Science and technology. Even though I don&apos;t really
+              understand how science works, I still enjoy reading articles and scientific journals. I am a person who
+              likes to learn new things, specific things cannot limit me in seeking knowledge, but what I mean here is
+              more emphasized in the field of technology.
             </p>
-            <h3 className='header-color'>Boring Stuff</h3>
-            <p className='max-w-2xl '>
-              I&apos;m not in front of my laptop all day, sometimes I go out to
-              get a fresh air and get in touch with my friends, and:
+            <h3 className="header-color">Hobbies</h3>
+            <p className="max-w-2xl ">
+              I&apos;m not in front of my laptop all day, sometimes I go out and get some fresh air, then hang out with
+              friends, and sometimes, I:
             </p>
-            <AnimeContainer
-              className='list-none grid grid-cols-2 xs:grid-cols-4 gap-2 md:gap-4'
-              delay={0.5}
-              list>
+            <AnimeContainer className="list-none grid grid-cols-2 xs:grid-cols-4 gap-2 md:gap-4" delay={0} list>
               {hobies.map((item, idx) => (
                 <HobbyCard key={idx + item.title} {...item} />
               ))}
             </AnimeContainer>
           </section>
 
-          <figure className='flex items-center justify-center'>
-            <div className='md:self-start scale-75 sm:scale-100'>
+          <figure className="flex items-center justify-center">
+            <div className="md:self-start scale-75 sm:scale-100">
               <NextImage
-                src='https://ik.imagekit.io/mlnzyx/personal_web-og/rizki_n-T6Lnn7f.webp?updatedAt=1639726869965'
-                alt='Rizki Maulana Citra'
+                src="https://ik.imagekit.io/mlnzyx/personal_web-og/rizki_n-T6Lnn7f.webp?updatedAt=1639726869965"
+                alt="Rizki Maulana Citra"
                 width={244}
                 height={244}
-                className='rounded'
+                className="rounded"
               />
             </div>
           </figure>
@@ -117,25 +103,15 @@ const IndexPage = ({ result }: IndexPageProps) => {
       </div>
 
       <section>
-        <AnimeContainer className='py-20'>
-          <h2 className='header-color'>Technical Skills</h2>
-          <p className='max-w-3xl md:text-lg'>
-            Speaking about skills, I honestly deal well with JavaScript, but my
-            first programming language that I&apos;m learned was Java back in
-            High School. But it turns out that after I&apos;ve graduated from
-            High School, I deal most of my time with Frontend environment which
-            are HTML/CSS/JS, and now JavaScript are my main programming
-            language.
+        <AnimeContainer className="py-20">
+          <h2 className="header-color">Technical Skills</h2>
+          <p className="max-w-3xl md:text-lg">
+            Some of my Technical Skills in Web Development. To be honest, the first programming language I learned was
+            Java, when I was still in high school. But apparently, when I graduated, I was learning more about Frontend
+            Environment, which are HTML, CSS and JavaScript, and Now my main Programming Language is TypeScript followed
+            by JavaScript.
           </p>
-          <p className='max-w-3xl md:text-lg'>
-            Once again I&apos;m not limiting myself to only some tech, hey I
-            love to learn new things!. Althought not everything are
-            &quot;good&quot; with JavaScript, but full-stack JavaScript are my
-            future goal.
-          </p>
-          <AnimeContainer
-            delay={0.5}
-            className='grid md:grid-cols-2 flex-[1_1_auto] gap-2 md:gap-4'>
+          <AnimeContainer delay={0.5} className="grid md:grid-cols-2 flex-[1_1_auto] gap-2 md:gap-4">
             {TechincalSkill.map((data, idx) => (
               <SkillCard key={idx + data.title} {...data} />
             ))}
@@ -143,15 +119,13 @@ const IndexPage = ({ result }: IndexPageProps) => {
         </AnimeContainer>
       </section>
 
-      <div className='py-20 md:py-40'>
+      <div className="py-20 md:py-40">
         <AnimeContainer>
           <section>
-            <h2 className='header-color'>Projects</h2>
-            <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-              List of my projects, I&apos;m always looking for new challenges
-              and I am always trying to improve myself. Below are list of my
-              projects, some of them are just for fun tho, feel free to check
-              them out.
+            <h2 className="header-color">Projects</h2>
+            <p className="max-w-3xl 2xl:text-lg mt-2 md:mt-4">
+              I am always looking for new challenges, and always trying to improve myself. Here&apos;s a list of my
+              projects you might be interested in
             </p>
           </section>
           <AnimeContainer
@@ -159,29 +133,29 @@ const IndexPage = ({ result }: IndexPageProps) => {
             className={clsx(
               'grid sm:grid-cols-2 md:grid-cols-3 flex-[1_1_auto]',
               'gap-4 gap-y-6 md:gap-6 md:gap-y-8 mt-2 md:mt-4'
-            )}>
+            )}
+          >
             {result.data && result.data.length > 0 && (
               <>
                 {result.data
                   .filter((item) => item.attributes.featured)
                   .map((prop) => (
-                    <ProjectCard
-                      {...prop}
-                      key={prop.id + prop.attributes.title}
-                    />
+                    <ProjectCard {...prop} key={prop.id + prop.attributes.title} />
                   ))}
                 <div
-                  title='see more of my projects on GitHub'
+                  title="see more of my projects on GitHub"
                   className={clsx(
                     'w-full h-56 sm:h-auto',
                     'transition-all duration-150 border-2 border-dashed rounded cursor-pointer',
                     'border-dark-300 dark:border-dark-700 hover:border-dark-500 dark:hover:border-dark-500'
-                  )}>
+                  )}
+                >
                   <NextLink
-                    className='flex items-center justify-center w-full h-full'
-                    href='https://github.com/rizkimcitra?tab=repositories'>
-                    <span className='sr-only'>See more projects on GitHub</span>
-                    <IoLogoGithub className='text-[2.5em] md:text-[3em]' />
+                    className="flex items-center justify-center w-full h-full"
+                    href="https://github.com/rizkimcitra?tab=repositories"
+                  >
+                    <span className="sr-only">See more projects on GitHub</span>
+                    <IoLogoGithub className="text-[2.5em] md:text-[3em]" />
                   </NextLink>
                 </div>
               </>
@@ -190,30 +164,38 @@ const IndexPage = ({ result }: IndexPageProps) => {
         </AnimeContainer>
       </div>
 
-      <div className='space-y-8 md:space-y-24 py-20 md:py-40'>
+      <div className="space-y-8 md:space-y-24 py-20 md:py-40">
         <section>
-          <h2 className='mb-2 md:mb-4 header-color'>Article</h2>
-          <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-            I&apos;m planning to write an article about my personal opinion,
-            Internet, Web Development, and my daily life, if you&apos;re
-            interested, you can check them out later, as I&apos;m currently
-            still making it happens, it&apos;s just a little bit slow because
-            currently I have a lot of things to do for college.
+          <h2 className="mb-2 md:mb-4 header-color">Article</h2>
+          <p className="max-w-3xl 2xl:text-lg mt-2 md:mt-4">
+            I plan to write articles that will talk about Information Technology, Web Development, and my daily life. If
+            you&apos;re interested, you can visit it later, because I&apos;m currently a little busy with college
           </p>
         </section>
         <section>
-          <h2 className='mb-2 md:mb-4 header-color'>Contact</h2>
-          <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-            Hey, wanna talk something with me? I&apos;m always open to new
-            topics, if you are interested to talk with me, feel free to reach me
-            out at my email or my social media account. You can see them in the
-            footer below.
+          <h2 className="mb-2 md:mb-4 header-color">Contact</h2>
+          <p className="max-w-3xl 2xl:text-lg mt-2 md:mt-4">
+            If you have any questions and want to contact me, please contact me via email, social media is where I learn
+            and share.
           </p>
+          <NextLink
+            href="mailto:rmaulana.citra@gmail.com"
+            className={clsx(
+              'flex items-center h-8 md:h-10 max-w-max',
+              'px-2 md:px-4 rounded transition-all space-x-2 md:space-x-3 bg-gradient-to-r',
+              'from-purple-500 via-primary-500 to-sky-500 text-white',
+              'dark:from-red-500 dark:via-rose-500 dark:to-primary-500',
+              'hover:opacity-75 active:opacity-100 active:grayscale'
+            )}
+          >
+            <IoMail />
+            <span>Email me</span>
+          </NextLink>
         </section>
       </div>
       <Footer ref={footerRef} />
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,10 +1,5 @@
-import {
-  IoMail,
-  IoLogoGithub,
-  IoLogoLinkedin,
-  IoLogoFacebook
-} from 'react-icons/io5'
-import { TechincalType } from '@/types/customType'
+import { IoMail, IoLogoGithub, IoLogoLinkedin, IoLogoFacebook } from 'react-icons/io5';
+import { TechincalType } from '@/types/customType';
 
 /**
  * @description Routes, defines the routes of the pages, if you add a new route the page would also updated
@@ -18,7 +13,7 @@ export const routes = [
     path: '/article',
     name: 'Article'
   }
-]
+];
 
 /**
  * @description links mean your social media, and this would be used in the `Footer` component, look up to the `src/components/Footer.tsx`.
@@ -45,7 +40,7 @@ export const links = [
     url: 'https://www.facebook.com/rizkimcitraa',
     Icon: IoLogoFacebook
   }
-]
+];
 
 /**
  * @description an object of object, this is the meta data of each page, currently just for mockup purpose.
@@ -55,17 +50,14 @@ export const metaPages = {
   home: {
     title: 'Home',
     description: `I am a 18 Y.O flexible guy who loves to learn new things, I like to read article about science and technology. I couldn't help with the science tho, because I have no idea how it works, but I still love to read and learn a bit about it.`,
-    imageURL:
-      'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
+    imageURL: 'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
     imageALT: 'Rizki Maulana Citra Home page',
     url: 'https://rizkicitra.my.id'
   },
   article: {
     title: 'Article',
-    description:
-      'In this page you can see my article, I often to write about my daily life and about my hobbies.',
-    imageURL:
-      'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
+    description: 'In this page you can see my article, I often to write about my daily life and about my hobbies.',
+    imageURL: 'https://ik.imagekit.io/mlnzyx/personal_web-og/homepage_8O1cNMK7I.png?updatedAt=1639665256919',
     imageALT: 'Rizki Maulana Citra Article page',
     url: 'https://rizkicitra.my.id/article'
   },
@@ -91,7 +83,7 @@ export const metaPages = {
       content: 'Rizki Maulana Citra'
     }
   ]
-}
+};
 
 /**
  * @description This is a constant for the type of Technical Skills, all those skills are stored here,
@@ -103,26 +95,35 @@ export const TechincalSkill: Array<TechincalType> = [
     title: 'Markup and Stylesheet',
     Icon: ['markdown', 'html', 'css', 'sass', 'tailwindcss'],
     description: [
-      `I have 1 Year experience with HTML and CSS, I have also learn SASS to improve my writing stylesheet, but my main tools of using Stylesheet is TailwindCSS.`,
+      `I have experience with HTML and CSS for 1 Year, then I also learned SASS as a super power of native CSS, and now, the main CSS Framework that I use to style websites is using Tailwind CSS.`,
       'Markdown is a simple way to write text that can be converted to HTML, and everyone know how to use it.'
     ]
   },
   {
     title: 'JavaScript and TypeScript',
     Icon: ['js', 'ts'],
-    description: `I have 1 Year experience with JavaScript, and about 3 Months with TypeScript, TypeScript is very heplful when it comes to writing code, it static type feature are awesome, and I am currently using it`
+    description: [
+      `I learned JavaScript about 10 Months ago, and I quite like this language, then TypeScript came along, I then learned TypeScript about 3 Months ago.`,
+      `TypeScript is very helpful in the development process, the static typing feature is the feature I like the most.`
+    ]
   },
   {
     title: 'React and Next.js',
     Icon: ['react', 'nextjs'],
-    description: `I have 10 Months experience with React, and about 6 Months with Next.js, Next.js is a framework that is very easy to use, and I am currently using it as my main go to frontend framework.`
+    description: [
+      `React is the coolest Library I've ever used, React is a library for building user interfaces, I learned React in June 2021.`,
+      `And Next.js is a React Framework that brings very cool features to developers, such as SSG, and SSR, I learned Next.js in August 2021, and Next.js has become my main Framework building an app.`
+    ]
   },
   {
     title: 'Backend Environment',
-    Icon: ['nodejs', 'express', 'strapi', 'php'],
-    description: `Although this should be not here, I've recently learning and doing some backend development, even though it's on localhost, I'm made my first API with express and I'm still learning to become a Fullstack JavaScript.`
+    Icon: ['nodejs', 'express'],
+    description: [
+      `Well, I don't think this should be here, but even so, I'm familiar with Node.js, and Express`,
+      `I started learning Node.js and Express.js in October 2021, but Backend environment doesn't seem as easy as I thought, I need to be more enthusiastic, and until now, I'm learning Node.js fundamentals`
+    ]
   }
-]
+];
 
 /**
  * @description list of hobies, you can add your own or even modify this, such as adding description or whatever you want
@@ -145,4 +146,4 @@ export const hobies = [
     title: 'Playing Games',
     icon: 'gamepad'
   }
-]
+];
