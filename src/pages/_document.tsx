430b45b38a67d8324d8650eb501811e5f8366f9e
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-24 15:46:21 +0700

    feat(src/pages): adding inter font from google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,12 @@ export default class CustomDocument extends Document {
           <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
