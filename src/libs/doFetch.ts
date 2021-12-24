0c39600d0f8682957e1cd9d3406c1c7c8876e351
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-24 15:54:46 +0700

    feat(src/libs): modifying doGet function from module doFetch
    as the type definiton got updated, this funcition need to get updated, the return value also got
    updated

--- src/libs/doFetch.ts
+++ src/libs/doFetch.ts
@@ -1,10 +1,5 @@
 import Axios from 'axios'
-import type {
-  HTTPGetType,
-  MetaStrapiType,
-  ProjectType,
-  SingleProjectType
-} from '@/types/customType'
+import type { HTTPGet } from '@/types/customType'
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL
 
@@ -20,22 +15,16 @@ const axios = Axios.create({
  * a function that returns a promise that resolves to the data of projects or articles or even specific data depends on the endpoint passed
  * @param path path of the endpoint, `e.g. /projects or /articls`
  */
-export const doGet: HTTPGetType = async (path) => {
-  if (!path) throw new Error('Path is required!, \n example: /user/bla')
+export const doGet: HTTPGet = async <T>(path: string) => {
+  if (!path) throw new Error('path is not defined')
   try {
-    const res = await axios.get(path)
-    const data: ProjectType = res.data
+    const res = await axios.get<T>(path)
     return {
-      isError: false,
-      result: data
+      result: res.data
     }
   } catch (error) {
     return {
-      isError: true,
-      result: {
-        data: [] as SingleProjectType[],
-        meta: {} as MetaStrapiType
-      }
+      result: [] as unknown as T
     }
   }
 }
