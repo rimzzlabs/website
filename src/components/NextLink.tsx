f051d0833ffe422d1dd8ae1c929d2b8048e39640
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-24 15:56:53 +0700

    feat(src/components): adding more if condition that return a button instead of anchor element
    also adding only Next.js Link component, that doesn't have children of anchor, it will be used if
    passHref param is there

--- src/components/NextLink.tsx
+++ src/components/NextLink.tsx
@@ -8,16 +8,25 @@ import type { NextLinkProps } from '@/types/customType'
  * 2. if href string start with `#` it will have a regular anchor tag
  * 3. if none above condition were met, this will return an anchor tag that contains property of `target="_blank"` and `rel="noopener noreferrer"`
  */
-const NextLink = ({
-  href,
-  children,
-  className = '',
-  unstyled = false,
-  ...props
-}: NextLinkProps) => {
-  const unstyledClass = unstyled
-    ? 'underline decoration-primary-500 dark:decoration-rose-500 underline-offset-1'
-    : ''
+const NextLink = ({ href, children, className = '', unstyled = false, ...props }: NextLinkProps) => {
+  const unstyledClass = unstyled ? 'underline decoration-primary-500 dark:decoration-rose-500 underline-offset-1' : ''
+
+  if (props.passHref) {
+    return (
+      <Link href={href} scroll={false} passHref {...props}>
+        {children}
+      </Link>
+    )
+  }
+
+  if (props.smooth) {
+    return (
+      <button onClick={props.onClick} className={clsx(className, unstyledClass)}>
+        {children}
+      </button>
+    )
+  }
+
   if (href.startsWith('/')) {
     return (
       <Link href={href} scroll={false}>
@@ -37,12 +46,7 @@ const NextLink = ({
   }
 
   return (
-    <a
-      href={href}
-      className={clsx(className, unstyledClass)}
-      target='_blank'
-      rel='noopener noreferrer'
-      {...props}>
+    <a href={href} className={clsx(className, unstyledClass)} target='_blank' rel='noopener noreferrer' {...props}>
       {children}
     </a>
   )
