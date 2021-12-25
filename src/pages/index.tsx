45d8b51a756d3d9a5fca0c0a88cec4fa618664c1
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-25 20:06:31 +0700

    feat(src/pages): add new list of articles
    modifying layout, articles will show up before project list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,24 +13,34 @@ import { hobies, metaPages, TechincalSkill } from '@/utils/constant'
 import { useRef } from 'react'
 import { doGet } from '@/libs/doFetch'
 import { IoLogoGithub, IoMail } from 'react-icons/io5'
-import type { ProjectProps, SingleProjectType } from '@/types/customType'
+import type { ArticleProps, ProjectProps, SingleArticleType, SingleProjectType } from '@/types/customType'
 import type { GetStaticProps } from 'next'
+import ArticleCard from '@/components/cards/ArticleCard'
+import readingTime from 'reading-time'
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await doGet<ProjectProps>('/project?sort=id:DESC')
+  const projectsRes = await doGet<ProjectProps>('/project?sort=id:DESC')
+  const articlesRes = await doGet<ArticleProps>('/article?sort=id:DESC')
+
+  const articles = articlesRes.result.data.map((item) => {
+    const estRead = readingTime(item.attributes.content)
+    return { ...item, estRead }
+  })
   return {
     props: {
-      data: response.result?.data
+      projects: projectsRes.result.data,
+      articles
     },
-    revalidate: 15
+    revalidate: 1
   }
 }
 
 type IndexPageProps = {
-  data: Array<SingleProjectType>
+  projects: Array<SingleProjectType>
+  articles: Array<SingleArticleType>
 }
 
-const IndexPage = ({ data }: IndexPageProps) => {
+const IndexPage = ({ projects, articles }: IndexPageProps) => {
   const footerRef = useRef<HTMLDivElement>(null)
   const scrollToRef = useSmoothScroll(footerRef)
 
@@ -61,52 +71,50 @@ const IndexPage = ({ data }: IndexPageProps) => {
         </section>
       </FullPage>
 
-      <div className='py-20 md:py-40'>
-        <AnimeContainer className={clsx('flex flex-col-reverse w-full', 'md:flex-row md:justify-between')}>
-          <section className='w-full md:pr-4 lg:pr-0'>
-            <h2 className='header-color'>About Me</h2>
-            <p className='max-w-3xl 2xl:text-lg'>
-              My name is Rizki Maulana Citra, I was born in Pandeglang Banten. If you don&apos;t know Banten, you may
-              have heard of{' '}
-              <NextLink href='https://en.wikipedia.org/wiki/Ujung_Kulon_National_Park' unstyled>
-                Ujung Kulon National Park
-              </NextLink>
-              .
-            </p>
-            <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-              I am an 18 year old who is passionate about Science and technology. Even though I don&apos;t really
-              understand how science works, I still enjoy reading articles and scientific journals. I am a person who
-              likes to learn new things, specific things cannot limit me in seeking knowledge, but what I mean here is
-              more emphasized in the field of technology.
-            </p>
-            <h3 className='header-color'>Hobbies</h3>
-            <p className='max-w-2xl '>
-              I&apos;m not in front of my laptop all day, sometimes I go out and get some fresh air, then hang out with
-              friends, and sometimes, I:
-            </p>
-            <AnimeContainer className='list-none grid grid-cols-2 xs:grid-cols-4 gap-2 md:gap-4' delay={0} list>
-              {hobies.map((item, idx) => (
-                <HobbyCard key={idx + item.title} {...item} />
-              ))}
-            </AnimeContainer>
-          </section>
+      <AnimeContainer className={clsx('flex flex-col-reverse w-full py-20 md:py-40', 'md:flex-row md:justify-between')}>
+        <section className='w-full md:pr-4 lg:pr-0'>
+          <h2 className='header-color'>About Me</h2>
+          <p className='max-w-3xl 2xl:text-lg'>
+            My name is Rizki Maulana Citra, I was born in Pandeglang Banten. If you don&apos;t know Banten, you may have
+            heard of{' '}
+            <NextLink href='https://en.wikipedia.org/wiki/Ujung_Kulon_National_Park' unstyled>
+              Ujung Kulon National Park
+            </NextLink>
+            .
+          </p>
+          <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
+            I am an 18 year old who is passionate about Science and technology. Even though I don&apos;t really
+            understand how science works, I still enjoy reading articles and scientific journals. I am a person who
+            likes to learn new things, specific things cannot limit me in seeking knowledge, but what I mean here is
+            more emphasized in the field of technology.
+          </p>
+          <h3 className='header-color'>Hobbies</h3>
+          <p className='max-w-2xl '>
+            I&apos;m not in front of my laptop all day, sometimes I go out and get some fresh air, then hang out with
+            friends, and sometimes, I:
+          </p>
+          <AnimeContainer className='list-none grid grid-cols-2 xs:grid-cols-4 gap-2 md:gap-4' delay={0} list>
+            {hobies.map((item, idx) => (
+              <HobbyCard key={idx + item.title} {...item} />
+            ))}
+          </AnimeContainer>
+        </section>
 
-          <figure className='flex items-center justify-center'>
-            <div className='md:self-start scale-75 sm:scale-100'>
-              <NextImage
-                src='https://ik.imagekit.io/mlnzyx/personal_web-og/rizki_n-T6Lnn7f.webp?updatedAt=1639726869965'
-                alt='Rizki Maulana Citra'
-                width={244}
-                height={244}
-                className='rounded'
-              />
-            </div>
-          </figure>
-        </AnimeContainer>
-      </div>
+        <figure className='flex items-center justify-center'>
+          <div className='md:self-start scale-75 sm:scale-100'>
+            <NextImage
+              src='https://ik.imagekit.io/mlnzyx/personal_web-og/rizki_n-T6Lnn7f.webp?updatedAt=1639726869965'
+              alt='Rizki Maulana Citra'
+              width={244}
+              height={244}
+              className='rounded'
+            />
+          </div>
+        </figure>
+      </AnimeContainer>
 
-      <section>
-        <AnimeContainer className='py-20'>
+      <AnimeContainer className='py-20 -scroll-mt-96'>
+        <section>
           <h2 className='header-color'>Technical Skills</h2>
           <p className='max-w-3xl md:text-lg'>
             Some of my Technical Skills in Web Development. To be honest, the first programming language I learned was
@@ -119,83 +127,94 @@ const IndexPage = ({ data }: IndexPageProps) => {
               <SkillCard key={idx + data.title} {...data} />
             ))}
           </AnimeContainer>
-        </AnimeContainer>
-      </section>
+        </section>
+      </AnimeContainer>
 
-      <div className='py-20 md:py-40'>
-        <AnimeContainer>
-          <section>
-            <h2 className='header-color'>Projects</h2>
-            <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-              I am always looking for new challenges, and always trying to improve myself. Here&apos;s a list of my
-              projects you might be interested in
-            </p>
-          </section>
-          <AnimeContainer
-            delay={0.5}
-            className={clsx(
-              'grid sm:grid-cols-2 md:grid-cols-3 flex-[1_1_auto]',
-              'gap-4 gap-y-6 md:gap-6 md:gap-y-8 mt-2 md:mt-4'
-            )}
-          >
-            {data && data.length > 0 ? (
-              <>
-                {data
-                  .filter((item) => item.attributes.featured)
-                  .map((prop) => (
-                    <ProjectCard {...prop} key={prop.id + prop.attributes.title} />
-                  ))}
-                <div
-                  title='see more of my projects on GitHub'
-                  className={clsx(
-                    'w-full h-56 sm:h-auto',
-                    'transition-all duration-150 border-2 border-dashed rounded cursor-pointer',
-                    'border-dark-300 dark:border-dark-700 hover:border-dark-500 dark:hover:border-dark-500'
-                  )}
-                >
-                  <NextLink
-                    className='flex items-center justify-center w-full h-full'
-                    href='https://github.com/rizkimcitra?tab=repositories'
-                  >
-                    <span className='sr-only'>See more projects on GitHub</span>
-                    <IoLogoGithub className='text-[2.5em] md:text-[3em]' />
-                  </NextLink>
-                </div>
-              </>
-            ) : null}
-          </AnimeContainer>
-        </AnimeContainer>
-      </div>
-
-      <div className='space-y-8 md:space-y-24 py-20 md:py-40'>
+      <AnimeContainer className='space-y-8 md:space-y-24 py-20 md:py-40 -scroll-mt-96'>
         <section>
           <h2 className='mb-2 md:mb-4 header-color'>Article</h2>
           <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-            I plan to write articles that will talk about Information Technology, Web Development, and my daily life. If
-            you&apos;re interested, you can visit it later, because I&apos;m currently a little busy with college
+            I&apos;m not a good writer, but sometimes I write to share my knowledge or even express my personal opinion,
+            I cover some topic about Web Development, Internet and Social life based on my personal view.
           </p>
-        </section>
-        <section>
-          <h2 className='mb-2 md:mb-4 header-color'>Contact</h2>
-          <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
-            If you have any questions and want to contact me, please contact me via email, social media is where I learn
-            and share.
-          </p>
-          <NextLink
-            href='mailto:rmaulana.citra@gmail.com'
-            className={clsx(
-              'flex items-center h-8 md:h-10 max-w-max',
-              'px-2 md:px-6 rounded transition-all space-x-2 md:space-x-3 bg-gradient-to-r',
-              'from-purple-500 via-primary-500 to-sky-500 text-white',
-              'dark:from-red-500 dark:via-rose-500 dark:to-primary-500',
-              'hover:opacity-75 active:opacity-100 active:grayscale'
-            )}
+          <AnimeContainer
+            list
+            delay={0.4}
+            className={clsx('grid sm:grid-cols-2', 'w-full flex-[1_1_auto] gap-4 md:gap-8 mt-4 md:mt-8')}
           >
-            <IoMail />
-            <span>Email me</span>
-          </NextLink>
+            {articles.length > 0 &&
+              articles.map((data, idx) => (
+                <li key={idx} className='min-h-[8rem]'>
+                  <ArticleCard {...data} />
+                </li>
+              ))}
+          </AnimeContainer>
         </section>
-      </div>
+      </AnimeContainer>
+
+      <AnimeContainer className='py-20 md:py-40 -scroll-mt-96'>
+        <section>
+          <h2 className='header-color'>Projects</h2>
+          <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
+            I am always looking for new challenges, and always trying to improve myself. Here&apos;s a list of my
+            projects you might be interested in
+          </p>
+        </section>
+        <AnimeContainer
+          delay={0.5}
+          className={clsx(
+            'grid sm:grid-cols-2 md:grid-cols-3 flex-[1_1_auto]',
+            'gap-4 gap-y-6 md:gap-6 md:gap-y-8 mt-2 md:mt-4'
+          )}
+        >
+          {projects.length > 0 && (
+            <>
+              {projects
+                .filter((item) => item.attributes.featured)
+                .map((prop) => (
+                  <ProjectCard {...prop} key={prop.id + prop.attributes.title} />
+                ))}
+              <div
+                title='see more of my projects on GitHub'
+                className={clsx(
+                  'w-full h-56 sm:h-auto',
+                  'transition-all duration-150 border-2 border-dashed rounded cursor-pointer',
+                  'border-dark-300 dark:border-dark-700 hover:border-dark-500 dark:hover:border-dark-500'
+                )}
+              >
+                <NextLink
+                  className='flex items-center justify-center w-full h-full'
+                  href='https://github.com/rizkimcitra?tab=repositories'
+                >
+                  <span className='sr-only'>See more projects on GitHub</span>
+                  <IoLogoGithub className='text-[2.5em] md:text-[3em]' />
+                </NextLink>
+              </div>
+            </>
+          )}
+        </AnimeContainer>
+      </AnimeContainer>
+
+      <section className='py-20 -scroll-mt-96'>
+        <h2 className='mb-2 md:mb-4 header-color'>Contact</h2>
+        <p className='max-w-3xl 2xl:text-lg mt-2 md:mt-4'>
+          If you have any questions and want to contact me, please contact me via email, social media is where I learn
+          and share.
+        </p>
+        <NextLink
+          href='mailto:rmaulana.citra@gmail.com'
+          className={clsx(
+            'flex items-center h-8 md:h-10 max-w-max',
+            'px-2 md:px-6 rounded transition-all space-x-2 md:space-x-3 bg-gradient-to-r',
+            'from-purple-500 via-primary-500 to-sky-500 text-white',
+            'dark:from-red-500 dark:via-rose-500 dark:to-primary-500',
+            'hover:opacity-75 active:opacity-100 active:grayscale'
+          )}
+        >
+          <IoMail />
+          <span>Email me</span>
+        </NextLink>
+      </section>
       <Footer ref={footerRef} />
     </>
   )
