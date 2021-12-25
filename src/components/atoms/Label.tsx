43a91fc2d7dfcbaca91c17f91cfdee0ecc806900
Rizki Maulana Citra <62492410+rizkimcitra@users.noreply.github.com>
2021-12-25 15:04:12 +0700

    feat(src/components/atoms): Add new css if statement

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -11,7 +11,7 @@ const Label = ({ type }: LabelProps) => {
   if (type === 'React' || type === 'Reactjs') classNames = 'bg-sky-100 text-sky-800'
   if (type === 'WebDev' || type === 'webdev') classNames = 'bg-teal-100 text-teal-800'
   if (type === 'General' || type === 'general') classNames = 'bg-neutral-100 text-neutral-800'
-
+  if (type === 'css' || type === 'CSS') classNames = 'bg-blue-100 text-blue-700'
   return <span className={clsx('p-1 px-2 text-xs sm:text-sm select-none rounded', classNames)}>{type}</span>
 }
 
