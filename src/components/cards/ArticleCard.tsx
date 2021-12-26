a6cd6fb258c2d7251c53d232bdc05c0e61bd8c4b
rizkimcitra <rmaulana.citra@gmail.com>
2021-12-26 19:34:19 +0700

    feat(src/components/cards): remove image for web optimization

--- src/components/cards/ArticleCard.tsx
+++ src/components/cards/ArticleCard.tsx
@@ -1,6 +1,5 @@
 import type { SingleArticleType } from '@/types/customType'
 
-import NextImage from '../NextImage'
 import NextLink from '../NextLink'
 import Label from '../atoms/Label'
 
@@ -12,32 +11,21 @@ const ArticleCard = ({ attributes, estRead }: SingleArticleType) => {
     <NextLink
       href={linkToPost}
       className={clsx(
-        'group flex flex-col items-center justify-center',
-        'w-full h-full overflow-hidden',
-        'border rounded transition-all duration-75',
-        'bg-white dark:bg-dark-900 border-dark-300 dark:border-dark-700',
-        'hover:border-dark-400 dark:hover:border-dark-600'
+        'flex flex-col items-center justify-center',
+        'w-full h-full rounded overflow-hidden',
+        'border border-color transition-all duration-200'
       )}
     >
       <div className='w-full h-full p-2 md:p-4'>
-        <figure className='relative aspect-[2/1]'>
-          <NextImage
-            fit='cover'
-            className='rounded'
-            src={attributes.image.src}
-            alt={`Image from Unsplash by - ${attributes.image.author}`}
-            layoutFill
-          />
-          <div className='absolute bottom-0 left-0 bg-gradient-to-t from-black/80 to-white/0 w-full h-1/5' />
-          <div className='absolute bottom-1 left-1 flex items-center space-x-2 md:space-x-3'>
+        <section className='px-1.5 md:px-2 mt-2 md:mt-4'>
+          <div className='flex items-center space-x-2 md:space-x-3'>
             {attributes.labels && attributes.labels.map((item, idx) => <Label type={item} key={idx + item} />)}
           </div>
-        </figure>
-
-        <section className='px-1.5 md:px-2 mt-2 md:mt-4'>
-          <div className='flex items-center justify-between'>
-            <h3 className='my-2 md:my-4'>{attributes.title}</h3>
-            <span>{estRead.text}</span>
+          <div className='flex items-center justify-between '>
+            <h3 className='my-2 md:my-4 max-w-[14rem] md:max-w-full group-hover:decoration-primary-500 dark:group-hover:decoration-rose-500'>
+              {attributes.title}
+            </h3>
+            <span className='text-xs sm:text-sm'>{estRead.text}</span>
           </div>
           <p className='max-w-2xl mb-1.5 md:mb-3'>{attributes.description}</p>
         </section>
