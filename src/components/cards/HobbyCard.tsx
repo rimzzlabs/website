a598b09dce145b5e0cfa93eac5fd8b6c091058bc
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-12-29 14:39:59 +0700

    feat(src/components/cards): modifying border

--- src/components/cards/HobbyCard.tsx
+++ src/components/cards/HobbyCard.tsx
@@ -6,15 +6,9 @@ import clsx from 'clsx'
 
 const HobbyCard = ({ icon, title }: HobbyCardProps) => {
   return (
-    <li
-      className={clsx(
-        'flex items-center space-x-2 p-2 md:p-4',
-        'min-h-[4rem] rounded border',
-        'border-color transition-all duration-200'
-      )}
-    >
-      <Icon className='text-[1.25em] md:text-[1.5em] text-primary-500 dark:text-rose-500' type={icon} />
-      <p className='text-typo-600 dark:text-typo-400'>{title}</p>
+    <li className={clsx('flex items-center space-x-2 p-2 md:p-4', 'min-h-[4rem] rounded border', 'border-color')}>
+      <Icon className='text-[1.25em] md:text-[1.5em] text-primary-500 dark:text-primary-400' type={icon} />
+      <p className='text-typo-600 dark:text-typo-400 text-xs sm:text-sm md:text-base'>{title}</p>
     </li>
   )
 }
