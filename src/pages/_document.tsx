454664ca79b50cdc2501b6699a0de1e54b07ee20
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-12-29 15:18:32 +0700

    feat(src/pages): remove fonts google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,10 @@ export default class CustomDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta name='theme-color' content='#171717' />
+          <meta name='theme-color' content='#3b82f6' />
           <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
-            rel='stylesheet'
-          />
         </Head>
         <body>
           <Main />
