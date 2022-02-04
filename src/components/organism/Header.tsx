ba3c64da7b1268578c158f7d36bd79d0df54f8e8
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-04 16:38:20 +0700

    feat(/src/components/mollecules): adjust height, and backdrop blur

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -3,8 +3,8 @@ import Nav from '../mollecules/Nav'
 import clsx from 'clsx'
 
 const Header = () => (
-  <div className={clsx('fixed inset-0', 'h-12 md:h-16', 'backdrop-blur', 'bg-theme-50/50 dark:bg-theme-900/50')}>
-    <div className={clsx('h-1 md:h-2 w-full', 'bg-gradient-to-r', 'from-indigo-500 to-primary-500')} />
+  <div className={clsx('fixed inset-0', 'h-16 md:h-24', 'backdrop-blur z-50', 'bg-theme-50/50 dark:bg-theme-900/50')}>
+    <div className={clsx('h-2 w-full', 'bg-gradient-to-r', 'from-primary-500 to-ternary-500')} />
     <header className={clsx('layout h-full', 'flex items-center justify-between')}>
       <Nav />
     </header>
