97e4ab1212ad8a316d3d7e60ef757379216a6337
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-07 10:25:46 +0700

    feat(/components/organism): conditionally rendering

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,16 +1,23 @@
-import DarkMode from '../mollecules/DarkMode'
 import Nav from '../mollecules/Nav'
 
 import clsx from 'clsx'
+import { useRouter } from 'next/router'
 
-const Header = () => (
-  <div className={clsx('fixed inset-0', 'h-16 md:h-24', 'backdrop-blur z-50', 'bg-theme-50/50 dark:bg-theme-900/50')}>
-    <div className={clsx('h-2 w-full', 'bg-gradient-to-r', 'from-primary-500 to-ternary-500')} />
-    <header className={clsx('layout h-full', 'flex items-center justify-between')}>
-      <Nav />
-      <DarkMode />
-    </header>
-  </div>
-)
+const Header = () => {
+  const { pathname } = useRouter()
+
+  const isError = pathname === '/_error' || pathname === '/404'
+
+  if (isError) return null
+
+  return (
+    <div className={clsx('fixed inset-0', 'h-16 md:h-20', 'backdrop-blur z-50', 'bg-theme-50/50 dark:bg-theme-900/50')}>
+      <div className={clsx('h-2 w-full', 'bg-gradient-to-r', 'from-primary-500 to-ternary-500')} />
+      <header className={clsx('layout h-full pb-2', 'flex items-center justify-between')}>
+        <Nav />
+      </header>
+    </div>
+  )
+}
 
 export default Header
