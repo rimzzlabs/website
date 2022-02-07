84a31766da568fa018580a9b48105bd4d916693c
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-07 10:27:21 +0700

    feat(/src/pages): add page transition with framer motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,34 @@
 import '@/styles/globals.css'
+import '@/styles/prism.css'
 
 import Header from '@/components/organism/Header'
 
+import variants, { withExit } from '@/libs/animation/variants'
+
+import { AnimatePresence, LazyMotion, Variants, domAnimation, m } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const v: Variants = withExit(variants)
+
+const App = ({ Component, pageProps, router }: AppProps) => {
   return (
-    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
-      <Header />
-      <div className='layout mt-16 md:mt-24'>
-        <Component {...pageProps} />
-      </div>
+    <ThemeProvider attribute='class' storageKey='theme' enableSystem>
+      <LazyMotion features={domAnimation}>
+        <Header />
+        <AnimatePresence initial={false} onExitComplete={() => window.scrollTo(0, 0)} exitBeforeEnter>
+          <m.div
+            key={router.route.concat('1')}
+            variants={v}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            className='layout'
+          >
+            <Component {...pageProps} />
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </ThemeProvider>
   )
 }
