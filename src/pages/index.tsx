8d146cdbdddf12680c1f75f8f6c76532d49dda79
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-07 10:28:06 +0700

    feat(src/pages): expand UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,80 @@
 import Image from '@/components/atoms/Image'
+import ArticleCard from '@/components/mollecules/ArticleCard'
+import ProjectCard from '@/components/mollecules/ProjectCard'
+import Footer from '@/components/organism/Footer'
+import Section from '@/components/organism/Section'
+import Layout from '@/components/templates/Layout'
 
-import portfolio from '@/data/portfolio'
-import PageLayout from '@/layouts/PageLayout'
+import { ArticleHeadProps } from '@/data/articles/articleType'
+import { PortfolioHeadProps } from '@/data/portfolio/portfolioType'
+import dateFormat from '@/libs/dateFormat'
+import { getArticle, getPortfolio } from '@/libs/mdx'
 
 import clsx from 'clsx'
 import { NextPage } from 'next'
-import { IoGlobe, IoLogoGithub } from 'react-icons/io5'
 
-const IndexPage: NextPage = () => {
+interface HomePageProps {
+  portfolios: Array<PortfolioHeadProps>
+  articles: Array<ArticleHeadProps>
+}
+
+export const getStaticProps = async () => {
+  const [portfolioRes, articleRes] = await Promise.all([getPortfolio(), getArticle()])
+
+  const portfolios = portfolioRes
+    .filter((data) => data.featured)
+    .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1))
+    .map((data) => {
+      const date = dateFormat(data.date)
+
+      return { ...data, date }
+    })
+
+  const articles = articleRes
+    .filter((data) => data.featured)
+    .sort((a, b) => (new Date(a.publishedAt) < new Date(b.publishedAt) ? 1 : -1))
+    .map((data) => {
+      const publishedAt = dateFormat(data.publishedAt)
+      return { ...data, publishedAt }
+    })
+
+  return {
+    props: {
+      portfolios,
+      articles
+    }
+  }
+}
+
+const HomePage: NextPage<HomePageProps> = ({ portfolios = [], articles = [] }) => {
+  const meta = {
+    title: 'Rizki Maulana Citra',
+    description: 'Frontend Developer, Student and Remote Worker'
+  }
   return (
-    <PageLayout title='Home'>
+    <Layout {...meta}>
       <div
         className={clsx(
-          'flex flex-col',
+          'flex flex-col pb-10 md:pb-20',
           'md:flex-row-reverse md:justify-between md:items-center',
-          'py-10 md:py-20 space-y-4 md:space-y-0'
+          'space-y-4 md:space-y-0'
         )}
       >
         <figure className={clsx('flex items-center md:justify-end self-start', 'w-full md:w-2/6', 'mb-4 md:mb-0')}>
           <Image
-            width={140}
-            height={140}
+            width={144}
+            height={144}
             layout='intrinsic'
             alt='Rizki Maulana Citra'
             className='rounded-full'
-            src='https://ik.imagekit.io/rizkicitra/rizkicitra_dev/profile_ou7tfIev4.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1643735467105'
+            src='/static/profile.webp'
           />
         </figure>
         <section>
           <h1>Rizki M Citra</h1>
           <p
             className={clsx(
-              'max-w-max mt-2 md:mt-4 mb-3 md:mb-6',
+              'max-w-max my-3 md:my-4',
               'text-transparent font-bold text-lg md:text-xl',
               'bg-clip-text bg-gradient-to-r',
               'from-primary-500 to-ternary-500'
@@ -39,48 +82,36 @@ const IndexPage: NextPage = () => {
           >
             Frontend Developer
           </p>
-          <p className='md:text-lg max-w-prose mb-2 md:mb-4'>
+          <p className='max-w-prose mb-2 md:mb-4'>
             Howdy👋, I&apos;m Rizki Maulana Citra, a Frontend Developer who loves to code, music, and drink coffee to
-            fullfill his energy, talks about React, Next.js, JavaScript and CSS.
+            fullfill his energy, talks about React, Next.js, JavaScript and CSS
           </p>
         </section>
       </div>
-      <section className={clsx('py-10 md:py-20')}>
-        <h2 className={clsx('mb-4 md:mb-6')}>Portfolios</h2>
 
-        <div className={clsx('grid flex-1 w-full', 'grid-cols-1 md:grid-cols-2', 'gap-4 md:gap-6')}>
-          {portfolio.map((portf, index) => (
-            <div key={portf.title + index} className={clsx('p-2 md:p-4', 'rounded border border-theme-700')}>
-              {/* <figure className={clsx('relative', 'w-full aspect-video')}>
-                <Image src={portf.src} alt={portf.title} className='rounded' objectFit='cover' />
-              </figure> */}
-              <h3>{portf.title}</h3>
-              <p className={clsx('my-2 md:my-4')}>{portf.description}</p>
+      <Section
+        title='Featured Portfolio'
+        Component={ProjectCard}
+        data={portfolios}
+        link={{
+          children: 'All portfolios',
+          to: '/portfolio'
+        }}
+      />
 
-              <div className={clsx('flex items-center justify-end space-x-2 md:space-x-3')}>
-                <a
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href={portf.url.github}
-                  className={clsx('inline-flex items-center justify-center', 'text-xl md:text-2xl')}
-                >
-                  <IoLogoGithub />
-                </a>
-                <a
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href={portf.url.live}
-                  className={clsx('inline-flex items-center justify-center', 'text-xl md:text-2xl')}
-                >
-                  <IoGlobe />
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    </PageLayout>
+      <Section
+        title='Featured Article'
+        Component={ArticleCard}
+        data={articles}
+        link={{
+          children: 'See more articles',
+          to: '/article'
+        }}
+      />
+
+      <Footer />
+    </Layout>
   )
 }
 
-export default IndexPage
+export default HomePage
