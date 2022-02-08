4e9049bd9e72682d9f6656fa0956fb1582d04573
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-08 15:30:50 +0700

    feat(/components/organism): modify bg opacity
    from 50 to 80

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   if (isError) return null
 
   return (
-    <div className={clsx('fixed inset-0', 'h-16 md:h-20', 'backdrop-blur z-50', 'bg-theme-50/50 dark:bg-theme-900/50')}>
+    <div className={clsx('fixed inset-0', 'h-16 md:h-20', 'backdrop-blur z-50', 'bg-theme-50/80 dark:bg-theme-900/80')}>
       <div className={clsx('h-2 w-full', 'bg-gradient-to-r', 'from-primary-500 to-ternary-500')} />
       <header className={clsx('layout h-full pb-2', 'flex items-center justify-between')}>
         <Nav />
