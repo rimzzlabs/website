5a97165ffab3fd9505104f5ec3ad3d908a6b0158
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-08 16:12:51 +0700

    feat(/components/atoms): add skip component
    used to skip keyboard accessibillity to prevent keyboard clicked nav and just gooooo to main content

---
+++ src/components/atoms/Skip.tsx
@@ -0,0 +1,24 @@
+import { Link } from '@/components/atoms/Link'
+
+import clsx from 'clsx'
+
+const Skip: React.FC = () => {
+  return (
+    <Link
+      className={clsx(
+        'accessible',
+        'absolute left-[20%] top-4',
+        'inline-flex items-center justify-center',
+        'py-2 px-4 rounded text-sm',
+        'bg-gradient-to-r z-[999]',
+        '-translate-y-96 focus-visible:-translate-y-0',
+        'text-white from-primary-500 to-ternary-500'
+      )}
+      href='#skip-content'
+    >
+      Skip to content
+    </Link>
+  )
+}
+
+export default Skip
