9c93edb8bd89a8f0eb41e115203a9a32ee00e392
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-10 23:41:59 +0700

    feat(components/atoms): update interface, null coalescing
    add null coalescing if there is templateTitle property available, and exnted interface from
    NextSeoProps

--- src/components/atoms/Seo.tsx
+++ src/components/atoms/Seo.tsx
@@ -1,11 +1,14 @@
 import { NextSeo, NextSeoProps } from 'next-seo'
 
+export interface SeoProp extends NextSeoProps {
+  templateTitle?: string
+}
 /**
  * It takes a NextSeoProps object and returns a `NextSeo components`
  * @returns A Next.js component.
  */
-const Seo: React.FC<NextSeoProps> = ({ ...props }) => {
-  const TITLE_TEMPLATE = '%s | Student and Frontend Developer from Indonesia'
+const Seo: React.FC<SeoProp> = ({ ...props }) => {
+  const TITLE_TEMPLATE = `%s - ${props.templateTitle ?? 'Student and Frontend Developer from Indonesia'}`
   return <NextSeo {...props} title={props.title} titleTemplate={TITLE_TEMPLATE} />
 }
 
