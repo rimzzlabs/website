de6ae50601e01dd932ed93ad17d215dd908574d2
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-11 10:13:54 +0700

    feat(comopnents/mollecules): on ArticleCard and ProjectCard
    change the heading element from h4 to h3

--- src/components/mollecules/ArticleCard.tsx
+++ src/components/mollecules/ArticleCard.tsx
@@ -6,7 +6,7 @@ import clsx from 'clsx'
 
 const ArticleCard: React.FC<ArticleHeadProps> = ({ title, slug, topics }) => (
   <div className={clsx('flex flex-col relative p-4', 'border rounded border-theme-300 dark:border-theme-700')}>
-    <h4 className='mb-2 md:mb-4'>{title}</h4>
+    <h3 className='mb-2 md:mb-4'>{title}</h3>
     <div className={clsx('flex items-center space-x-2 md:space-x-3')}>
       {topics.length > 0 &&
         topics.map((topic, index) => (
--- src/components/mollecules/ProjectCard.tsx
+++ src/components/mollecules/ProjectCard.tsx
@@ -9,7 +9,7 @@ import clsx from 'clsx'
 const ProjectCard = ({ title, summary, slug, stack }: PortfolioHeadProps) => {
   return (
     <div className={clsx('flex flex-col relative p-4', 'border rounded border-theme-300 dark:border-theme-700')}>
-      <h4>{title}</h4>
+      <h3>{title}</h3>
       <p className={'my-3 md:my-4'}>{summary}</p>
       <div className={clsx('flex items-center mt-auto space-x-1 md:space-x-2')}>
         {stack.length > 0 && stack.map((item, index) => <IconFinder type={item} key={item + index} />)}
