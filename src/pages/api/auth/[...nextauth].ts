65ee1ebf0accbe3655054c4ec0cfd8cae9745100
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-12 12:48:53 +0700

    feat(auth/): add secret key

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
+  secret: process.env.SECRET_KEY,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
