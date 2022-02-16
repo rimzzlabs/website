a24e381e52b69aa557571e84ba3031222f4ce9dd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-16 11:29:14 +0700

    feat(pages/portfolio): remove searchbar

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -9,7 +9,6 @@ import { getPortfolio } from '@/libs/mdx'
 
 import clsx from 'clsx'
 import { NextPage } from 'next'
-import React, { useState } from 'react'
 
 export const getStaticProps = async () => {
   const res = await getPortfolio()
@@ -30,8 +29,6 @@ interface ProjectPageProps {
 }
 
 const ProjectPage: NextPage<ProjectPageProps> = ({ portfolios = [] }) => {
-  const [searchVal, setSearchVal] = useState<string>('')
-
   const meta = {
     title: 'Portfolio',
     templateTitle: 'Rizki Maulana Citra, Student and Frontend Developer',
@@ -39,28 +36,11 @@ const ProjectPage: NextPage<ProjectPageProps> = ({ portfolios = [] }) => {
       "List of my personal portfolio, proven that I've created something with my knowledge and experience, and like any other people, I will grow my skill and combine it with experience i have."
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchVal(e.target.value)
-
   return (
     <Layout {...meta}>
       <Hero {...meta} />
 
-      <div className='py-10'>
-        <div className={clsx('flex items-stretch', 'h-10')}>
-          <input
-            className={clsx(
-              'w-full px-4 text-sm',
-              'outline-none bg-transparent rounded',
-              'bg-theme-50 border border-theme-300 dark:border-transparent text-theme-700 dark:bg-theme-800 dark:text-theme-200'
-            )}
-            placeholder='Search portfolios'
-            type='text'
-            value={searchVal}
-            onChange={handleChange}
-          />
-        </div>
-      </div>
-      <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'flex-1 gap-4 md:gap-5')}>
+      <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'flex-1 gap-4 md:gap-5', 'pt-10 md:pt-20')}>
         {portfolios.length > 0 &&
           portfolios
             .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1))
