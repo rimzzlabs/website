4c304d68cb889637eaaafbc36cbfe812b065acc6
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-19 03:21:28 +0700

    docs(src/data/): update description each post

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   darkMode: 'class', // or 'media' or 'class' or false
   theme: {
     extend: {
