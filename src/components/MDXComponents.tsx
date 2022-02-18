b010f763ef10626a62479b3df4090b9496eab721
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-19 03:20:35 +0700

    feat(src/components/): update MDXComponent as the ArticleLink added recently

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,11 +1,15 @@
+import ArticleLink from '@/components/atoms/ArticleLink'
 import Image from '@/components/atoms/Image'
 import Link from '@/components/atoms/Link'
 import BlogImage from '@/components/mollecules/BlogImage'
+import Pre from '@/components/mollecules/Pre'
 
 const MDXComponents = {
   Link,
   Image,
-  BlogImage
+  BlogImage,
+  ArticleLink,
+  pre: Pre
 }
 
 export default MDXComponents
