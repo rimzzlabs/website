97298ac19fc7c2911a00c7cec7b501a8550ee146
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 00:02:36 +0700

    feat(pages/article/): update UI, add ArticleCard and meta tags

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,7 +1,6 @@
-// import ArticleCard from '@/components/mollecules/ArticleCard'
-import Link from '@/components/atoms/Link'
+import ArticleCard from '@/components/mollecules/ArticleCard'
 import Hero from '@/components/mollecules/Hero'
-// import Footer from '@/components/organism/Footer'
+import Footer from '@/components/organism/Footer'
 import Layout from '@/components/templates/Layout'
 
 import { ArticleHeadProps } from '@/data/articles/articleType'
@@ -10,6 +9,12 @@ import { getArticle } from '@/libs/mdx'
 
 import clsx from 'clsx'
 import { GetStaticProps, NextPage } from 'next'
+
+const meta = {
+  title: 'Article',
+  description:
+    'I write articles once in a while, talks about JavaScript, CSS, and React Ecosystem and Web Development related topics, I have no high quality article like what you expect, but somehow I like to share my knowledge and experience throught writing.'
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const res = await getArticle()
@@ -31,30 +36,15 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-const ArticlePage: NextPage<{ articles: Array<ArticleHeadProps> }> = () => {
+const ArticlePage: NextPage<{ articles: Array<ArticleHeadProps> }> = ({ articles = [] }) => {
   return (
-    <Layout title='Article'>
-      <Link
-        href='/'
-        className={clsx(
-          'inline-flex mb-2 md:mb-4',
-          'text-theme-600 hover:text-theme-800',
-          'dark:text-theme-500 dark:hover:text-theme-300'
-        )}
-      >
-        &larr; Back to Home
-      </Link>
+    <Layout {...meta}>
+      <Hero {...meta} />
 
-      <Hero
-        title='Under Construction'
-        description='This page still in development, sooner or later will be commited to the main branch, you can check this page later😃.'
-        className='mb-2 md:mb-4'
-      />
-
-      {/* <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'pt-10 flex-1 gap-4 md:gap-5')}>
+      <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'pt-10 flex-1 gap-4 md:gap-5')}>
         {articles.length > 0 && articles.map((data, index) => <ArticleCard key={data.title + index} {...data} />)}
-      </div> */}
-      {/* <Footer /> */}
+      </div>
+      <Footer />
     </Layout>
   )
 }
