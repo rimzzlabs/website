b2a62d433b6fe274b8a8d81ad236251a386b8da9
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 00:00:41 +0700

    feat(src/pages/): add searchbar, live filtering, and re layouting
    add searchbar to provide user to search item, and live filtering the portfolio item

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -5,22 +5,33 @@ import Layout from '@/components/templates/Layout'
 
 import { PortfolioHeadProps } from '@/data/portfolio/portfolioType'
 import dateFormat from '@/libs/dateFormat'
-import { getPortfolio } from '@/libs/mdx'
 
+import axios from 'axios'
 import clsx from 'clsx'
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
+import React, { useState } from 'react'
+import { HiOutlineSearch } from 'react-icons/hi'
 
-export const getStaticProps = async () => {
-  const res = await getPortfolio()
+const meta = {
+  title: 'Portfolio',
+  description:
+    "List of my personal portfolio, proven that I've created something with my knowledge and experience, and like any other people, I will grow my skill and combine it with experience i have."
+}
 
-  const portfolios = res.map((data) => {
-    const date = dateFormat(data.date)
-    return { ...data, date }
-  })
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await axios.get<{ data: Array<PortfolioHeadProps> }>('http://localhost:3000/api/portfolio')
+
+  const portfolios =
+    data.data.map((data) => {
+      const date = dateFormat(data.date)
+      return { ...data, date }
+    }) ?? []
+
   return {
     props: {
       portfolios
-    }
+    },
+    revalidate: 60
   }
 }
 
@@ -29,23 +40,51 @@ interface ProjectPageProps {
 }
 
 const ProjectPage: NextPage<ProjectPageProps> = ({ portfolios = [] }) => {
-  const meta = {
-    title: 'Portfolio',
-    templateTitle: 'Rizki Maulana Citra, Student and Frontend Developer',
-    description:
-      "List of my personal portfolio, proven that I've created something with my knowledge and experience, and like any other people, I will grow my skill and combine it with experience i have."
-  }
+  const [query, setQuery] = useState('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)
+
+  const filteredPortfolios = portfolios.filter(
+    (data) =>
+      data.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+      data.summary.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  )
 
   return (
     <Layout {...meta}>
       <Hero {...meta} />
-
-      <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'flex-1 gap-4 md:gap-5', 'pt-10 md:pt-20')}>
-        {portfolios.length > 0 &&
-          portfolios
-            .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1))
-            .map((data, index) => <ProjectCard key={data.title + index} {...data} />)}
+      <div
+        className={clsx(
+          'w-full my-10 md:my-16 rounded transition-all',
+          'bg-transparent border border-theme-300',
+          'dark:border-transparent dark:bg-theme-800',
+          'focus-within:ring ring-primary-500 dark:ring-primary-400'
+        )}
+      >
+        <div className='flex items-center'>
+          <HiOutlineSearch className='w-12' />
+          <input
+            onChange={handleChange}
+            value={query}
+            type='text'
+            placeholder='Search...'
+            className={clsx('w-full h-12 text-sm', 'bg-transparent outline-none')}
+          />
+        </div>
       </div>
+
+      {portfolios.length > 0 && filteredPortfolios.length > 0 ? (
+        <div className={clsx('grid grid-cols-1 md:grid-cols-2', 'flex-1 gap-4 md:gap-5')}>
+          {filteredPortfolios.map((data, index) => (
+            <ProjectCard key={data.title + index} {...data} />
+          ))}
+        </div>
+      ) : null}
+
+      {filteredPortfolios.length === 0 && (
+        <div className='w-full text-center'>
+          <p>😞Oops, could not found what you are looking for....</p>
+        </div>
+      )}
       <Footer />
     </Layout>
   )
