c0d2fa34935329596cfec954e3fd9ecf9b5ecad8
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 00:03:50 +0700

    feat(pages/article/): show image, update UI, MDXRemote
    show image if there is any, update the UI, and passed MDXComponent to MDXRemote component

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -1,4 +1,6 @@
+import MDXComponents from '@/components/MDXComponents'
 import Image from '@/components/atoms/Image'
+import BlogImage from '@/components/mollecules/BlogImage'
 import Footer from '@/components/organism/Footer'
 import Layout from '@/components/templates/Layout'
 
@@ -56,40 +58,50 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 const ArticleDetailPage: NextPage<ArticleProps> = ({ frontMatter, mdxSource }) => {
   const date = dateFormat(frontMatter.publishedAt)
   return (
-    <Layout title={frontMatter.title} description={frontMatter.summary}>
-      <article className='prose dark:prose-invert'>
+    <Layout
+      title={frontMatter.title}
+      templateTitle='Article'
+      description={frontMatter.summary}
+      additionalMetaTags={[
+        {
+          name: 'keywords',
+          content: 'bruh, foo, bar, bizz'
+        },
+        {
+          name: 'Author',
+          content: frontMatter.author
+        }
+      ]}
+    >
+      <article className='prose dark:prose-invert max-w-2xl'>
         <header>
           <section className={clsx('border-b', 'border-theme-300 dark:border-theme-700')}>
             <h1>{frontMatter.title}</h1>
           </section>
-          <div
-            className={clsx(
-              'flex flex-col justify-between',
-              'md:flx-row md:items-center',
-              'w-full text-sm',
-              'space-x-2 md:space-x-4'
-            )}
-          >
-            <div className={clsx('flex items-center', 'space-x-2 md:space-x-4')}>
-              <figure>
-                <Image
-                  src={frontMatter.author_pfp}
-                  alt={frontMatter.author}
-                  className={clsx('rounded-full')}
-                  width={40}
-                  height={40}
-                  layout='intrinsic'
-                  priority
-                />
-              </figure>
-              <span>{frontMatter.author}</span>
+          <div className='flex flex-col md:flex-row'>
+            <div className={clsx('flex flex-col', 'items-start', 'w-full text-sm')}>
+              <div className={clsx('flex items-center', 'space-x-4')}>
+                <figure>
+                  <Image
+                    src={frontMatter.author_pfp}
+                    alt={frontMatter.author}
+                    className={clsx('rounded-full')}
+                    width={28}
+                    height={28}
+                    layout='intrinsic'
+                    priority
+                  />
+                </figure>
+                <span>{frontMatter.author}</span>
+              </div>
+              <span role='date'>{date}</span>
             </div>
-            <span className='justify-self-end'>{date}</span>
           </div>
         </header>
 
         <main>
-          <MDXRemote {...mdxSource} />
+          {frontMatter.image && <BlogImage src={frontMatter.image} alt='Photo From Unsplsh' />}
+          <MDXRemote {...mdxSource} components={MDXComponents} />
         </main>
       </article>
       <Footer />
