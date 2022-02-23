faf0ff23761d0fd3eb045a8302e4ddf209f4bcfe
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 00:11:43 +0700

    feat(components/mollecules/): add padding of 6 on medium viewport

--- src/components/mollecules/ArticleCard.tsx
+++ src/components/mollecules/ArticleCard.tsx
@@ -7,7 +7,7 @@ import Label from '../atoms/Label'
 import clsx from 'clsx'
 
 const ArticleCard: React.FC<ArticleHeadProps> = ({ title, slug, topics }) => (
-  <div className={clsx('flex flex-col relative p-4', 'border rounded border-theme-300 dark:border-theme-700')}>
+  <div className={clsx('flex flex-col relative p-4 md:p-6', 'border rounded border-theme-300 dark:border-theme-700')}>
     <h3 className='mb-2 md:mb-4'>{title}</h3>
     <div className={clsx('flex items-center mt-auto space-x-2 md:space-x-3')}>
       {topics.length > 0 && topics.map((topic, index) => <Label type={topic} key={topic + index} />)}
