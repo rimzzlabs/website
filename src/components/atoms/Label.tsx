417939779830824c9c02cf58b4edd179984e4b19
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-23 23:51:37 +0700

    feat(components/atoms/): create a new Label component
    used to display label on each ArticleCard, or even ProjectCad

---
+++ src/components/atoms/Label.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+
+interface LabelProps {
+  type: string
+  onClick?: () => void | (() => Promise<void>)
+}
+
+const Label: React.FC<LabelProps> = (props) => {
+  const baseClass = 'inline-flex items-center justify-center py-1 px-1.5 rounded text-xs md:text-sm'
+  switch (props.type) {
+    case 'devlife':
+      return (
+        <div className={clsx(baseClass, 'text-slate-700 dark:text-slate-200 bg-slate-100 dark:bg-slate-800')}>
+          {props.type}
+        </div>
+      )
+
+    default:
+      return (
+        <div className={clsx(baseClass, 'text-neutral-700 dark:text-neutral-200 bg-neutral-100 dark:bg-neutral-800')}>
+          {props.type}
+        </div>
+      )
+  }
+}
+
+export default Label
