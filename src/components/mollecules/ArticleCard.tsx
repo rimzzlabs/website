c26e2846960779ecbc6cdf384f4df7325fc7139e
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-23 23:57:40 +0700

    feat(components/mollecules/): add Label component
    this will show Label component instead of plain text

--- src/components/mollecules/ArticleCard.tsx
+++ src/components/mollecules/ArticleCard.tsx
@@ -2,18 +2,15 @@ import Link from '@/components/atoms/Link'
 
 import { ArticleHeadProps } from '@/data/articles/articleType'
 
+import Label from '../atoms/Label'
+
 import clsx from 'clsx'
 
 const ArticleCard: React.FC<ArticleHeadProps> = ({ title, slug, topics }) => (
   <div className={clsx('flex flex-col relative p-4', 'border rounded border-theme-300 dark:border-theme-700')}>
     <h3 className='mb-2 md:mb-4'>{title}</h3>
-    <div className={clsx('flex items-center space-x-2 md:space-x-3')}>
-      {topics.length > 0 &&
-        topics.map((topic, index) => (
-          <span className='text-xs md:text-sm' key={topic + index}>
-            {topic}
-          </span>
-        ))}
+    <div className={clsx('flex items-center mt-auto space-x-2 md:space-x-3')}>
+      {topics.length > 0 && topics.map((topic, index) => <Label type={topic} key={topic + index} />)}
     </div>
 
     <Link href={`/article/${slug}`} className={clsx('absolute inset-0 rounded')}>
