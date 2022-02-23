a6b3f90478f3c09c2893154a5b2f34da65b9c777
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-23 23:56:39 +0700

    style(components/atoms/): update style
    modify text color, bg color of after props

--- src/components/atoms/ArticleLink.tsx
+++ src/components/atoms/ArticleLink.tsx
@@ -9,12 +9,12 @@ const ArticleLink: React.FC<LinkProps> = ({ children, href, ...props }) => {
       href={href}
       className={clsx(
         'relative inline-flex items-center',
-        'border-b border-dotted no-underline',
-        'border-theme-500 dark:border-theme-600',
-        'text-theme-600 dark:text-theme-400 hover:text-primary-500 dark:hover:text-primary-400',
-        'after:absolute after:left-0 after:bottom-0',
-        'after:w-0 after:h-0.5 after:transition-all hover:after:w-full',
-        'after:bg-primary-500 dark:after:bg-primary-400'
+        'border-b border-dashed no-underline',
+        'border-theme-900 dark:border-theme-200',
+        'text-theme-700 dark:text-theme-400 hover:text-primary-500 dark:hover:text-primary-400',
+        'after:absolute after:left-0 after:-bottom-0.5',
+        'after:w-0 after:h-0.5 after:transition-all after:duration-300 hover:after:w-full',
+        'after:bg-gradient-to-r after:from-primary-500 after:to-ternary-500'
       )}
     >
       {children}
