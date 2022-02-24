b75c2f7852017ba4badfec1f4236376c0d66bfdf
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 21:38:15 +0700

    feat: add BackToTop component
    this would handle scrollToTop function if the user clicked, and only show up if the scroll position
    are greater than 200 pixels

---
+++ src/components/atoms/BackToTop.tsx
@@ -0,0 +1,44 @@
+import useWindowScroll from '@/hooks/useWindowScroll'
+import variants, { withExit } from '@/libs/animation/variants'
+
+import clsx from 'clsx'
+import { AnimatePresence, Variants, m } from 'framer-motion'
+import { HiOutlineArrowUp } from 'react-icons/hi'
+
+const BackToTop: React.FC = () => {
+  const yAxis = useWindowScroll()
+  const handleClick = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+  const v: Variants = withExit(variants)
+
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {yAxis > 200 ? (
+        <m.button
+          title='back to top'
+          variants={v}
+          initial='hidden'
+          animate='visible'
+          exit='exit'
+          onClick={handleClick}
+          className={clsx(
+            'accessible',
+            'fixed md:bottom-12 md:right-12',
+            'bottom-8 right-7',
+            'md:bottom-24 right-24',
+            'inline-flex items-center justify-center',
+            'w-10 md:w-12 h-10 md:h-12 z-10',
+            'rounded-md md:text-lg',
+            'text-primary-700 dark:text-primary-400',
+            'bg-primary-100 dark:bg-theme-800'
+          )}
+        >
+          <span className='sr-only'>Back To Top</span>
+          <HiOutlineArrowUp />
+        </m.button>
+      ) : null}
+    </AnimatePresence>
+  )
+}
+
+export default BackToTop
