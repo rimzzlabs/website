203f640c1a51ef88348c1071270597cdc7d7358b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 21:45:28 +0700

    feat(components/mdxcomponent.tsx): add blockquote, inline code and updae Pre to customPre

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,15 +1,19 @@
 import ArticleLink from '@/components/atoms/ArticleLink'
 import Image from '@/components/atoms/Image'
 import Link from '@/components/atoms/Link'
+import Blockquote from '@/components/mollecules/Blockquote'
 import BlogImage from '@/components/mollecules/BlogImage'
-import Pre from '@/components/mollecules/Pre'
+import CustomCode from '@/components/mollecules/CustomCode'
+import CustomPre from '@/components/mollecules/Pre'
 
 const MDXComponents = {
   Link,
   Image,
   BlogImage,
   ArticleLink,
-  pre: Pre
+  pre: CustomPre,
+  inlineCode: CustomCode,
+  blockquote: Blockquote
 }
 
 export default MDXComponents
