0bf1de7b0db89fa7874a05ac4f2541ed6b3232cf
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 22:02:13 +0700

    style: remove unused utilities

--- src/components/atoms/BackToTop.tsx
+++ src/components/atoms/BackToTop.tsx
@@ -25,7 +25,6 @@ const BackToTop: React.FC = () => {
             'accessible',
             'fixed md:bottom-[12vh] lg:right-[8vw]',
             'bottom-[6vh] right-[6vw]',
-            'md:bottom-24 right-24',
             'inline-flex items-center justify-center',
             'w-10 md:w-12 h-10 md:h-12 z-10',
             'rounded-md md:text-lg',
