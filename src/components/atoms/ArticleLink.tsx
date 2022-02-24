5721a0aaa9399e52d4088e699daa2c05ff48bee4
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-24 20:43:44 +0700

    feat(components/atoms): modify text color
    change text color from gray to primary color

--- src/components/atoms/ArticleLink.tsx
+++ src/components/atoms/ArticleLink.tsx
@@ -11,7 +11,7 @@ const ArticleLink: React.FC<LinkProps> = ({ children, href, ...props }) => {
         'relative inline-flex items-center',
         'border-b border-dashed no-underline',
         'border-theme-900 dark:border-theme-200',
-        'text-theme-700 dark:text-theme-400 hover:text-primary-500 dark:hover:text-primary-400',
+        'text-primary-500 dark:text-primary-400 font-semibold',
         'after:absolute after:left-0 after:-bottom-0.5',
         'after:w-0 after:h-0.5 after:transition-all after:duration-300 hover:after:w-full',
         'after:bg-gradient-to-r after:from-primary-500 after:to-ternary-500'
