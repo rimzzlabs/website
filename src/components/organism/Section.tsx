4b50257b189df538602c863aeded8e031bc95bbd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-25 22:45:09 +0700

    feat: wrap component inside Card component

--- src/components/organism/Section.tsx
+++ src/components/organism/Section.tsx
@@ -1,5 +1,7 @@
 import Link from '@/components/atoms/Link'
 
+import Card from '../atoms/Card'
+
 import clsx from 'clsx'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -19,9 +21,16 @@ const Section = <DataProp,>({ title, link, data, Component, gridCols }: SectionP
     <section className='py-10'>
       <h2 className='mb-2 md:mb-4'>{title}</h2>
       <div className={clsx('grid', 'flex-auto gap-4 md:gap-5 mb-2 md:mb-4', gridCols)}>
-        {data.length > 0 && data.map((data, index) => <Component key={index} {...data} />)}
+        {data.length > 0 &&
+          data.map((data, index) => (
+            <Card key={index}>
+              <Component {...data} />
+            </Card>
+          ))}
       </div>
-      <Link href={link.to ?? '/'}>{link.children ?? 'Go somewhere'} &rarr;</Link>
+      <Link href={link.to ?? '/'} className='text-theme-800 dark:text-theme-200'>
+        {link.children ?? 'Go somewhere'} &rarr;
+      </Link>
     </section>
   )
 }
