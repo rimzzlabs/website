d3736f8a399f9d860c47b2f143f7177796998206
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-25 20:58:15 +0700

    feat: add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,13 @@ export default class CustomDocument extends Document {
           <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
+          <script
+            async
+            defer
+            data-do-not-track='true'
+            data-website-id='6daf05f5-92d2-430f-9cdd-1801014260da'
+            src='https://rizkicitra-analytics.vercel.app/umami.js'
+          />
         </Head>
         <body>
           <Main />
