4954236c43f94452c44741489defdf5a8bc70408
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-02-25 23:33:58 +0700

    feat: modify font weight to be 400

--- src/components/mollecules/Nav.tsx
+++ src/components/mollecules/Nav.tsx
@@ -19,7 +19,7 @@ const Nav: React.FC = () => {
           href={route.path}
           className={clsx(
             'relative inline-flex text-sm md:text-base items-center justify-center',
-            'py-1 md:py-1.5 px-3 md:px-3.5 font-medium rounded transition-all',
+            'py-1 md:py-1.5 px-3 md:px-3.5 rounded transition-all',
             'hover:bg-primary-100 dark:hover:bg-theme-800',
             route.path === pathname ? 'text-primary-700 dark:text-primary-400' : 'text-theme-900 dark:text-theme-300'
           )}
