af01c569e599e08df22b968b7c981396a2afed89
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-14 13:46:31 +0700

    feat: remove className constant

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -3,23 +3,20 @@ import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 
 export type LinkProps = {
   className?: string
-  mergeClass?: boolean
   newTab?: boolean
 } & NextLinkProps
 
 const Link: React.FC<LinkProps> = ({ href, children, ...rest }) => {
-  const className = 'text-theme-500 hover:text-theme-900 dark:text-theme-400 dark:hover:text-primary-100'
-
   if (rest.newTab) {
     return (
-      <a rel='noopener noreferrer' target='_blank' href={href as string} className={clsx(rest?.className ?? className)}>
+      <a rel='noopener noreferrer' target='_blank' href={href as string} className={clsx(rest.className)}>
         {children}
       </a>
     )
   }
   return (
     <NextLink scroll={false} href={href} {...rest}>
-      <a className={clsx(rest?.className ?? className)}>{children}</a>
+      <a className={clsx(rest.className)}>{children}</a>
     </NextLink>
   )
 }
