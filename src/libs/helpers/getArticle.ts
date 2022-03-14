a8015a9c12ced42cb25324ae991432c37e771c54
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-14 13:55:33 +0700

    feat: add helpers function
    separate function to each module

---
+++ src/libs/helpers/getArticle.ts
@@ -0,0 +1,44 @@
+import { ArticleHeadProps } from '@/data/articles/articleType'
+import getFullPath, { ARTICLE_PATH } from '@/libs/helpers/getFullPath'
+
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+
+/**
+ * It reads the markdown file and returns the data and content.
+ * @param {string} slug - The slug is the name of the article file without the file extension.
+ */
+export const getArticleBySlug = async (slug: string) => {
+  const fullpath = path.join(`${ARTICLE_PATH}/articles`, `${slug}.mdx`)
+  const fileContent = fs.readFileSync(fullpath, 'utf8')
+
+  const { data, content } = matter(fileContent)
+
+  return {
+    data,
+    content
+  }
+}
+
+/**
+ * Get all articles from the `/articles` directory and return them as an array of objects
+ * @returns An array of objects that contain the matter data and the slug.
+ */
+const getArticle = async (): Promise<Array<ArticleHeadProps>> => {
+  const paths = getFullPath('/articles')
+  return paths.map((p) => {
+    const fullpath = path.join(`${ARTICLE_PATH}/articles`, p)
+    const fileContent = fs.readFileSync(fullpath, 'utf8')
+
+    const { data, content } = matter(fileContent)
+
+    return {
+      ...(data as ArticleHeadProps),
+      slug: p.replace('.mdx', ''),
+      content
+    }
+  })
+}
+
+export default getArticle
---
+++ src/libs/helpers/index.ts
@@ -0,0 +1,4 @@
+export { default as getPortfolio } from './getPortfolio'
+export { getPortfolioBySlug } from './getPortfolio'
+export { default as getArticle } from './getArticle'
+export { getArticleBySlug } from './getArticle'
