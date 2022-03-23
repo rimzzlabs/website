bde569242fd4d16eba688a3c30972c6a4dbc6235
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-23 15:59:37 +0700

    feat: using react-image-lightbox
    would display a lightbox if image was clicked

--- src/components/atoms/CustomImage.tsx
+++ src/components/atoms/CustomImage.tsx
@@ -1,5 +1,9 @@
 import Image from '@/components/atoms/Image'
 
+import { useState } from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
+
 interface CustomImageProps {
   alt: string
   src: string
@@ -7,18 +11,35 @@ interface CustomImageProps {
 }
 
 const CustomImage: React.FC<CustomImageProps> = ({ src, alt, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <Image
-      {...props}
-      src={src}
-      alt={alt}
-      layout='intrinsic'
-      width={672}
-      height={400}
-      className='rounded'
-      objectFit='cover'
-      loading='lazy'
-    />
+    <>
+      <Image
+        {...props}
+        onClick={() => setIsOpen(true)}
+        src={src}
+        alt={alt}
+        layout='intrinsic'
+        width={672}
+        height={400}
+        className='rounded cursor-pointer'
+        objectFit='cover'
+        loading='lazy'
+        placeholder='blur'
+        blurDataURL='/blur.svg'
+      />
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={src}
+          onCloseRequest={() => setIsOpen(false)}
+          reactModalStyle={{
+            maxWidth: '500px'
+          }}
+        />
+      )}
+    </>
   )
 }
 
