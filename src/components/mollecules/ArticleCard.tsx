ac1b5c30deaf5da34150edc69517dc4f959a0f27
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-28 14:54:29 +0700

    feat: add og props
    if the og props true, then the link used should be native from nextjs itself

--- src/components/mollecules/ArticleCard.tsx
+++ src/components/mollecules/ArticleCard.tsx
@@ -1,13 +1,17 @@
+import Label from '@/components/atoms/Label'
 import Link from '@/components/atoms/Link'
 
 import { ArticleHeadProps } from '@/data/articles/articleType'
 
-import Label from '../atoms/Label'
-
 import clsx from 'clsx'
+import NextLink from 'next/link'
 import readingTime from 'reading-time'
 
-const ArticleCard: React.FC<ArticleHeadProps> = ({ title, slug, topics, content }) => {
+interface ArticleCardProps extends ArticleHeadProps {
+  ogLink?: boolean
+}
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ title, slug, topics, content, ogLink }) => {
   const estRead = readingTime(content)
   return (
     <div className={clsx('relative flex flex-col p-3 h-full', 'bg-theme-50 dark:bg-theme-900')}>
@@ -23,9 +27,17 @@ const ArticleCard: React.FC<ArticleHeadProps> = ({ title, slug, topics, content 
         <span className='text-sm'>{estRead.text}</span>
       </div>
 
-      <Link href={`/article/${slug}`} className={clsx('absolute inset-0 rounded')}>
-        <span className='sr-only'>Read Article about project {title}</span>
-      </Link>
+      {ogLink ? (
+        <NextLink href={`/article/${slug}`}>
+          <a className={clsx('absolute inset-0 rounded')}>
+            <span className='sr-only'>Read Article about {title}</span>
+          </a>
+        </NextLink>
+      ) : (
+        <Link href={`/article/${slug}`} className={clsx('absolute inset-0 rounded')}>
+          <span className='sr-only'>Read Article about {title}</span>
+        </Link>
+      )}
     </div>
   )
 }
