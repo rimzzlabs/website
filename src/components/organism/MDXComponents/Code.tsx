d8cbd6e8c2e99f5c32dbab077464c296f138f360
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-28 14:50:31 +0700

    style: change text color to be primary 400

--- src/components/organism/MDXComponents/Code.tsx
+++ src/components/organism/MDXComponents/Code.tsx
@@ -6,7 +6,7 @@ const Code: React.FC = (props) => {
       className={clsx(
         'p-1 font-normal rounded border',
         'bg-transparent text-primary-600 border-theme-300',
-        'dark:border-theme-700 dark:text-primary-300'
+        'dark:border-theme-700 dark:text-primary-400'
       )}
     >
       <span>{props.children}</span>
