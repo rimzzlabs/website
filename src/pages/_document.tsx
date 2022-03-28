410ed85199f428d4cdfe572ecabb4c93d2da6502
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-03-28 14:59:09 +0700

    feat: conditionally add <script> tag if the envronments' are production

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,17 +5,19 @@ export default class CustomDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <meta name='theme-color' content='#171717' />
+          <meta name='theme-color' content='#18181b' />
           <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
           <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
-          <script
-            async
-            defer
-            data-do-not-track='true'
-            data-website-id='6daf05f5-92d2-430f-9cdd-1801014260da'
-            src='https://rizkicitra-analytics.vercel.app/umami.js'
-          />
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              async
+              defer
+              data-do-not-track='true'
+              data-website-id='6daf05f5-92d2-430f-9cdd-1801014260da'
+              src='https://rizkicitra-analytics.vercel.app/umami.js'
+            />
+          )}
         </Head>
         <body>
           <Main />
