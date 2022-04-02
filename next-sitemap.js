cccfa8cc9398a4f29f80fe33c6def6e625b50a92
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-02 16:06:18 +0700

    chore: update sitemap size to be 10000
    on next-config.js file

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://rizkicitra.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }]
-  }
+  },
+  sitemapSize: 10000
 }
