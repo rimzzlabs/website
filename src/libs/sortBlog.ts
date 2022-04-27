49c97a860636cf9544dc1cc2930ff675d4e1f1c1
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-28 04:27:19 +0700

    feat: sort blog by most viewed

--- src/libs/sortBlog.ts
+++ src/libs/sortBlog.ts
@@ -3,3 +3,7 @@ import { Blogs } from '@/data/blog/blog.type'
 export const getNewestBlog = (a: Blogs, b: Blogs) => {
   return new Date(a.published) < new Date(b.published) ? 1 : new Date(a.published) > new Date(b.published) ? -1 : 0
 }
+
+export const getMostPopularBlog = (a: Blogs, b: Blogs) => {
+  return a.views && b.views ? (a.views < b.views ? 1 : a.views > b.views ? -1 : 0) : 0
+}
