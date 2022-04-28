d572b68da05188577ec67df62ee480d334128645
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-28 21:34:23 +0700

    feat: render Header and Footer conditionally

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -4,17 +4,21 @@ import Footer from '@/components/organism/Footer'
 import { twclsx } from '@/libs/twclsx'
 
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 export interface LayoutProps extends CustomSeoProps {
   className?: string
 }
 
 const Layout: NextPage<LayoutProps> = ({ children, ...props }) => {
+  const { pathname } = useRouter()
+  const isError = pathname === '/_error' || pathname === '/_offline' || pathname === '/404'
+
   return (
     <>
       <Seo {...props} />
       <main className={twclsx('mt-36 scroll-mt-36')}>{children}</main>
-      <Footer />
+      {!isError && <Footer />}
     </>
   )
 }
