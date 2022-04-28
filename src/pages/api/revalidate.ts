678711817e249e60ee885a4af116a1d590a46b10
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-29 03:17:43 +0700

    feat: implement on-demand ISR

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const secret = req.query.secret as string
+  const envSecret = process.env.SECRET
+
+  if (secret !== envSecret) {
+    return res.status(401).send({
+      message: 'Invalid secret token'
+    })
+  }
+
+  try {
+    await res.unstable_revalidate('/blog')
+    return res.json({ revalidate: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send({
+      message: 'Error revalidating'
+    })
+  }
+}
