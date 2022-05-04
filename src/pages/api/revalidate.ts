8ea01e0d846c8ae3dc19016d73ea12c26d6db8a9
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-05-04 10:35:51 +0700

    style: modify env to next_public

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const secret = req.query.secret as string
-  const envSecret = process.env.SECRET
+  const envSecret = process.env.NEXT_PUBLIC_SECRET
 
   if (secret !== envSecret) {
     return res.status(401).send({
