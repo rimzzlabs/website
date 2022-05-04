9214a0bf73ff2c769cf32d08e9ef382283691e6d
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-05-04 10:35:22 +0700

    feat: add useEffect to revalidate On-Demand ISR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,29 @@ import Skip from '@/components/atoms/Skip'
 import Header from '@/components/organism/Header'
 
 import variants, { withExit } from '@/libs/animation/variants'
+import umamiClient from '@/libs/umamiClient'
 
 import { AnimatePresence, LazyMotion, Variants, domAnimation, m } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 const v: Variants = withExit(variants)
 
 const App = ({ Component, pageProps, router }: AppProps) => {
+  useEffect(() => {
+    const SECRET = process.env.NEXT_PUBLIC_SECRET
+    const isProd = process.env.NODE_ENV === 'production'
+
+    return () => {
+      ;(async () => {
+        // if it's on production on some condition fulfilled, run this HTTP request on component unmount
+        if (isProd) await umamiClient.get('/api/revalidate?secret=' + SECRET)
+      })()
+    }
+    // this useeffect will run everytime route change, except URL starts with /blog nor route /404
+  }, [router.route])
+
   return (
     <ThemeProvider attribute='class' storageKey='theme' enableSystem>
       <LazyMotion features={domAnimation}>
