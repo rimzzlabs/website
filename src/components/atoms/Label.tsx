fd19aca3c7b3404a0869246933f3e07dd186e09b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-05-30 11:49:44 +0700

    feat: add more label(s)

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -37,6 +37,34 @@ const Label: React.FunctionComponent<LabelProps> = (props) => {
         </div>
       )
 
+    case 'frontend':
+      return (
+        <div className={twclsx(baseClass, 'text-cyan-700 bg-cyan-100 dark:text-cyan-100 dark:bg-cyan-800')}>
+          {props.type}
+        </div>
+      )
+
+    case 'webdev':
+      return (
+        <div className={twclsx(baseClass, 'text-fuchsia-700 bg-fuchsia-100 dark:text-fuchsia-100 dark:bg-fuchsia-800')}>
+          {props.type}
+        </div>
+      )
+
+    case 'web analytics':
+      return (
+        <div className={twclsx(baseClass, 'text-emerald-700 bg-emerald-100 dark:text-emerald-100 dark:bg-emerald-800')}>
+          {props.type}
+        </div>
+      )
+
+    case 'hooks':
+      return (
+        <div className={twclsx(baseClass, 'text-blue-700 bg-blue-100 dark:text-blue-100 dark:bg-blue-800')}>
+          {props.type}
+        </div>
+      )
+
     default:
       return (
         <div className={twclsx(baseClass, 'text-neutral-700 dark:text-neutral-200 bg-neutral-100 dark:bg-neutral-800')}>
