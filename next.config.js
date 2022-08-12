242dad2db984dacd035a5980a0eee55321a1a2ce
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-08-12 17:41:57 +0700

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -21,10 +21,8 @@ module.exports = withPwa({
   },
   pwa: {
     runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
+    // buildExcludes: [/middleware-manifest.json$/],
     dest: 'public',
-    register: true,
-    skipWaiting: true,
     disable: process.env.NODE_ENV === 'development'
   }
 })
