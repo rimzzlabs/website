89c02deb27cca4ae49d94f20d53130a9bf4d566b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-17 20:18:28 +0700

    feat: try using next-pwa with different config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,42 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const withPWA = require('next-pwa')
 
-// const isDev = process.env.NODE_ENV === 'development'
+const runtimeCaching = require('next-pwa/cache')
+const isDev = process.env.NODE_ENV === 'development'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: isDev,
+  runtimeCaching,
+  buildExcludes: [
+    /chunks\/images\/.*$/, // Don't precache files under .next/static/chunks/images this improves next-optimized-images behaviour
+    /chunks\/pages\/api\/.*/ // Dont cache the API it needs fresh serverinfo
+  ],
+  exclude: [
+    /\.map$/, // dont cache map files
+    /^.*ts.*$/ // Dont let serviceworker touch the TS streams
+  ],
+  skipWaiting: true, // installs new SW when available without a prompt, we only need to send a reload request to user.
+  dynamicStartUrl: false, // recommend: set to false if your start url always returns same HTML document, then start url will be precached, this will help to speed up first load.
+  reloadOnOnline: false, // Prevents reloads on offline/online switch
+  sourcemap: false
+})
+
+/** @type {import('next').NextConfig} */
+const config = {
+  images: { domains: ['ik.imagekit.io', 'og-image.vercel.app', 'media3.giphy.com', 'media0.giphy.com'] },
+  compiler: { removeConsole: true },
+  swcMinify: true,
+  compress: true
+}
+
+module.exports = withPWA(config)
 
 // /** @type {import('next').NextConfig} */
-// module.exports = withPWA({
+// module.exports = {
 //   images: {
 //     domains: ['ik.imagekit.io', 'og-image.vercel.app', 'media3.giphy.com', 'media0.giphy.com']
 //   },
-//   pwa: {
-//     // buildExcludes: [/middleware-manifest.json$/],
-//     dest: 'public',
-//     disable: isDev
-//   }
-// })
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    domains: ['ik.imagekit.io', 'og-image.vercel.app', 'media3.giphy.com', 'media0.giphy.com']
-  },
-  compiler: {
-    removeConsole: true
-  },
-  swcMinify: true
-}
+//   compiler: {
+//     removeConsole: true
+//   },
+//   swcMinify: true
+// }
