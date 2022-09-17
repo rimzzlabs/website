cfd927b634008ed353ea0d6b78608976f5790357
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-17 20:26:58 +0700

    fix: using null coalescing and remove replaceAll function

--- src/libs/ogImage.ts
+++ src/libs/ogImage.ts
@@ -13,10 +13,10 @@ export const generateOgImage = (payload: generateOgImagePayload) => {
   return (
     'https://og-image.vercel.app/' +
     '**' +
-    (payload.title?.replaceAll(' ', '%20') ?? '') +
+    (payload?.title ?? '') +
     '**' +
     '%3Cbr%2F%3E' +
-    (payload.subTitle?.replaceAll(' ', '%20') ?? '') +
+    (payload?.subTitle ?? '') +
     '.png?theme=' +
     (payload.theme ?? 'dark') +
     '&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2F' +
