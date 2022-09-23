f248b8f2b8d4f771173af0906aa54535967c1407
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-24 03:01:47 +0700

    feat: enable remove console on next config

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const config = {
   images: { domains: ['ik.imagekit.io', 'og-image.vercel.app', 'media3.giphy.com', 'media0.giphy.com'] },
+  compiler: { removeConsole: !isDev },
   swcMinify: true,
   compress: true
 }
