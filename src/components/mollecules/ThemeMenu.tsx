c0ba4b302afe3377c2d8ffb8528399174de40938
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-23 22:23:45 +0700

    feat: add accessibillity to dropdown theme

--- src/components/mollecules/ThemeMenu.tsx
+++ src/components/mollecules/ThemeMenu.tsx
@@ -3,7 +3,7 @@ import { twclsx } from '@/libs/twclsx'
 
 import { m } from 'framer-motion'
 import type { Variants } from 'framer-motion'
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import { HiDesktopComputer, HiOutlineMoon, HiOutlineSun } from 'react-icons/hi'
 
 type ThemeMenuProps = {
@@ -13,13 +13,14 @@ type ThemeMenuProps = {
 }
 
 const ThemeMenu: React.FunctionComponent<ThemeMenuProps> = (props) => {
-  const ref = useRef<HTMLDivElement | null>(null)
-  useClickOutside(ref, props.onClose)
+  const animatableRef = useRef<HTMLDivElement | null>(null)
+  const listRef = useRef<HTMLUListElement | null>(null)
+  useClickOutside(animatableRef, props.onClose)
   const v = useMemo<Variants>(
     () => ({
-      hidden: { opacity: 0 },
-      enter: { opacity: 1 },
-      exit: { opacity: 0, translateY: '1rem' }
+      hidden: { opacity: 0, translateY: 25 },
+      enter: { opacity: 1, translateY: 0 },
+      exit: { opacity: 0, translateY: 20 }
     }),
     []
   )
@@ -38,73 +39,92 @@ const ThemeMenu: React.FunctionComponent<ThemeMenuProps> = (props) => {
   }, [props.theme, themesList])
 
   const handleKeyDown = useCallback(
-    (theme: string) => (e: React.KeyboardEvent<HTMLLIElement>) => {
-      switch (e.key) {
-        case ' ':
-        case 'SpaceBar':
-        case 'Enter':
-          e.preventDefault()
-          props.changeTheme(theme)
-          props.onClose()
-          break
-        default:
-          break
+    (theme: string, index: number) => (e: React.KeyboardEvent<HTMLLIElement>) => {
+      e.preventDefault()
+      const list = document.querySelectorAll("li[role='option']") as NodeListOf<HTMLLIElement>
+      const ARROW_UP = 'ArrowUp'
+      const ARROW_DOWN = 'ArrowDown'
+      const ARROW_LEFT = 'ArrowLeft'
+      const ARROW_RIGHT = 'ArrowRight'
+      const elArrowDown = list[index + 1]
+      const elArrowUp = list[index - 1]
+
+      const changeOpts = {
+        ' ': true,
+        SpaceBar: true,
+        Enter: true
+      } as Record<string, boolean>
+
+      if ((e.key === ARROW_DOWN || e.key === ARROW_RIGHT) && elArrowDown) {
+        elArrowDown.focus({ preventScroll: true })
+      } else if ((e.key === ARROW_DOWN || e.key === ARROW_RIGHT) && !elArrowDown) {
+        list[0].focus({ preventScroll: true })
+      } else if ((e.key === ARROW_UP || e.key === ARROW_LEFT) && elArrowUp) {
+        elArrowUp.focus({ preventScroll: true })
+      } else if ((e.key === ARROW_UP || e.key === ARROW_LEFT) && !elArrowUp) {
+        list[list.length - 1].focus({ preventScroll: true })
+      }
+
+      if (changeOpts[e.key]) {
+        props.changeTheme(theme)()
+        props.onClose()
       }
     },
     [props]
   )
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (listRef.current) {
+        ;(listRef.current.childNodes[0] as HTMLLIElement).focus({ preventScroll: true })
+      }
+    }
+  }, [])
+
   return (
     <m.div
-      ref={ref}
+      ref={animatableRef}
       initial='hidden'
       animate='enter'
       exit='exit'
       variants={v}
       className={twclsx(
         'absolute',
-        'right-0 -left-28 md:-left-28 top-16',
+        'right-0 -left-28 md:-left-28 top-14',
         'shadow-md dark:shadow-none rounded-lg',
         'bg-theme-50 dark:bg-theme-800'
       )}
     >
-      <div
-        className={twclsx(
-          'relative',
-          'before:absolute before:right-0 before:-top-[1.745rem]',
-          'before:border-t-[1rem] before:border-b-[1rem] before:border-l-[1rem] before:border-r-[1rem]',
-          'before:border-b-theme-100 before:border-r-transparent before:border-l-transparent before:border-t-transparent',
-          'dark:before:border-b-theme-800 before:z-[-1]'
-        )}
+      <ul
+        ref={listRef}
+        role='listbox'
+        aria-activedescendant={themesList[activeDecsendant].value}
+        tabIndex={-1}
+        className='flex flex-col rounded-lg p-2.5'
       >
-        <ul
-          role='listbox'
-          aria-activedescendant={themesList[activeDecsendant].value}
-          tabIndex={-1}
-          className='flex flex-col rounded-lg overflow-hidden'
-        >
-          {themesList.map((theme) => (
-            <li
-              onKeyDown={handleKeyDown(theme.value)}
-              role='option'
-              aria-selected={theme.value === props.theme}
-              tabIndex={0}
-              className={twclsx(
-                'inline-flex items-center w-full cursor-default',
-                'h-9 md:h-10 px-2.5 transition text-sm md:text-base font-semibold',
-                'hover:bg-theme-100 dark:hover:bg-theme-700',
-                'text-theme-700 dark:text-theme-200',
-                props.theme === theme.value && 'text-primary-700 dark:text-primary-500'
-              )}
-              key={theme.value}
-              onClick={props.changeTheme(theme.value)}
-            >
-              <theme.Icon className='mr-2.5' />
-              <span>{theme.name}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+        {themesList.map((theme, index) => (
+          <li
+            onAnimationStart={(e) => console.info(e)}
+            onKeyDown={handleKeyDown(theme.value, index)}
+            role='option'
+            aria-selected={theme.value === props.theme}
+            tabIndex={0}
+            className={twclsx(
+              'inline-flex items-center w-full cursor-default rounded-lg',
+              'h-9 md:h-10 px-2.5 transition text-sm md:text-base font-semibold',
+              'hover:bg-primary-100 dark:hover:bg-theme-700',
+              'text-theme-700 dark:text-theme-200',
+              'focus-visible:ring focus-visible:outline-none focus-visible:ring-primary-500',
+              props.theme === theme.value && 'text-primary-700 dark:text-primary-500'
+            )}
+            key={theme.value}
+            onClick={props.changeTheme(theme.value)}
+          >
+            <theme.Icon className='mr-2.5' />
+            <span>{theme.name}</span>
+          </li>
+        ))}
+      </ul>
     </m.div>
   )
 }
