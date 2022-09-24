78c552fcf0c8c8daa39996ae3fa6856688df9177
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-24 16:43:04 +0700

    fix: remove memo component

--- src/components/timeline/TimelineList.tsx
+++ src/components/timeline/TimelineList.tsx
@@ -2,14 +2,13 @@ import { twclsx } from '@/libs/twclsx'
 
 import { Timeline } from './Timeline'
 
-import { memo } from 'react'
 import type { Timeline as TimelineType } from 'rizkicitra'
 
 interface TimelineListProps {
   timeline: Array<TimelineType>
 }
 
-const Component: React.FunctionComponent<TimelineListProps> = ({ timeline }) => (
+export const TimelineList: React.FunctionComponent<TimelineListProps> = ({ timeline }) => (
   <ul className={twclsx('pl-2')}>
     {timeline
       .sort((a, b) => (new Date(a.start_date) < new Date(b.start_date) ? 1 : -1))
@@ -18,5 +17,3 @@ const Component: React.FunctionComponent<TimelineListProps> = ({ timeline }) => 
       ))}
   </ul>
 )
-
-export const TimelineList = memo(Component)
