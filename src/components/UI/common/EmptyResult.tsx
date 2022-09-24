e149b36162b335b710c5b35373f7f4e3d65531e4
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-09-24 21:30:20 +0700

    feat: create EmptyResult when search is none

---
+++ src/components/UI/common/EmptyResult.tsx
@@ -0,0 +1,12 @@
+import { CustomImage } from '@/UI/images'
+
+import { twclsx } from '@/libs/twclsx'
+
+export const EmptyResult: React.FunctionComponent = () => {
+  return (
+    <div className={twclsx('flex flex-col items-center justify-center')}>
+      <CustomImage src='/static/not-found.svg' display='intrinsic' width={256} height={256} alt='Not found' />
+      <p className='mt-2.5 md:mt-4'>Unfortunately, I got nothing to show more😞.</p>
+    </div>
+  )
+}
