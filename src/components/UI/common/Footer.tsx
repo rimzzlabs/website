8308780b63f9425a61ab99613e56b69b27fc0d82
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-10-02 23:57:07 +0700

    feat: revise footer

--- src/components/UI/common/Footer.tsx
+++ src/components/UI/common/Footer.tsx
@@ -1,4 +1,4 @@
-import { UnderlineLink, UnstyledLink } from '@/UI/links'
+import { UnstyledLink } from '@/UI/links'
 
 import APP_ROUTE, { ADDT_ROUTE } from '@/libs/constants/route'
 import SOCIAL from '@/libs/constants/social'
@@ -14,23 +14,15 @@ export const Footer: React.FunctionComponent = () => {
   const className = twclsx(
     'text-sm md:text-base md:max-w-max',
     'text-theme-500 hover:text-primary-500',
-    'dark:text-theme-400 dark:hover:text-primary-400'
+    'dark:text-theme-500 dark:hover:text-primary-500'
   )
-  const YEAR_NOW = new Date().getFullYear()
 
   if (isError) {
     return null
   }
 
   return (
-    <footer
-      className={twclsx(
-        'pb-4 md:pb-8 pt-6 md:pt-10',
-        'mt-10 md:mt-20',
-        'border-t',
-        'border-theme-300 dark:border-theme-700'
-      )}
-    >
+    <footer className={twclsx('py-6 md:py-10', 'mt-10 md:mt-20', 'border-t', 'border-theme-300 dark:border-theme-700')}>
       <section className={twclsx('flex items-center gap-2.5 mb-8')}>
         <div className={twclsx('inline-flex items-center', 'select-none')}>
           <CustomImage
@@ -54,7 +46,7 @@ export const Footer: React.FunctionComponent = () => {
         </div>
       </section>
 
-      <section className={twclsx('flex', 'mb-8 md:mb-12')}>
+      <section className={twclsx('flex')}>
         <div className={twclsx('flex flex-col', 'w-full', 'space-y-4')}>
           {APP_ROUTE.map((route) => (
             <UnstyledLink title={route.name} className={twclsx(className)} href={route.path} key={route.path}>
@@ -84,25 +76,6 @@ export const Footer: React.FunctionComponent = () => {
           ))}
         </div>
       </section>
-
-      <p className={twclsx('mb-2', 'text-sm font-medium', 'text-theme-500 dark:text-theme-400')}>
-        Built with &#128150; by{' '}
-        <UnderlineLink
-          title='Rizki Maulana Citras Github Profile'
-          className={twclsx(
-            'text-primary-600 hover:text-primary-500',
-            'dark:text-primary-400 dark:hover:text-primary-300',
-            'umami--click--github-profile-button'
-          )}
-          href='https://github.com/rizkimcitra'
-        >
-          Rizki M Citra
-        </UnderlineLink>
-      </p>
-
-      <p className={twclsx('text-sm font-medium text-theme-500', 'dark:text-theme-400')}>
-        &copy; 2021 - {YEAR_NOW} Under MIT License
-      </p>
     </footer>
   )
 }
