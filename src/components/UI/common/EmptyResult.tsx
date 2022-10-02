53d674b4cb6734b86945f2eb99ddc4367f5949fa
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-10-03 00:03:51 +0700

    feat: smaller text size on smaller screen

--- src/components/UI/common/EmptyResult.tsx
+++ src/components/UI/common/EmptyResult.tsx
@@ -6,7 +6,7 @@ export const EmptyResult: React.FunctionComponent = () => {
   return (
     <div className={twclsx('flex flex-col items-center justify-center')}>
       <CustomImage src='/static/not-found.svg' display='intrinsic' width={256} height={256} alt='Not found' />
-      <p className='mt-2.5 md:mt-4'>Unfortunately, I got nothing to show more😞.</p>
+      <p className='text-sm md:text-base mt-2.5 md:mt-4'>Unfortunately, I got nothing to show more😞.</p>
     </div>
   )
 }
