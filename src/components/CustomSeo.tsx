cfada6c1120bd7cf9c96cae92bfacf5faf897012
rizkimcitra <rmaulana.citra@gmail.com>
2022-10-14 11:21:30 +0700

    fix: undefined title when no env available (on local)

--- src/components/CustomSeo.tsx
+++ src/components/CustomSeo.tsx
@@ -11,6 +11,6 @@ const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
  * It takes a NextSeoProps object and returns a `<NextSeo /> component`.
  */
 export const CustomSeo: React.FunctionComponent<CustomSeoProps> = ({ ...props }) => {
-  const TITLE_TEMPLATE = `%s — ${props.template ?? SITE_NAME}`
+  const TITLE_TEMPLATE = `%s — ${props.template ?? (SITE_NAME || 'rizkicitra.dev')}`
   return <NextSeo {...props} title={props.title} titleTemplate={TITLE_TEMPLATE} />
 }
