901b991a022a914e1507f028559b7f96ab4065da
rizkimcitra <rmaulana.citra@gmail.com>
2022-10-20 00:56:27 +0700

    feat: update resume

--- src/libs/constants/resume.ts
+++ src/libs/constants/resume.ts
@@ -1,8 +1,9 @@
 import SOCIAL from './social'
 
+import { Experience } from 'rizkicitra'
+
 export const HEADLINE = {
-  name: 'RIZKI MAULANA CITRA',
-  role: 'Frontend Engineer'
+  name: 'Rizki Maulana Citra'
 }
 
 export const LINKS: typeof SOCIAL = [
@@ -21,15 +22,15 @@ export const SKILLS = [
   {
     name: 'Technologies',
     list: [
-      'React',
-      'Vite',
+      'React.js',
       'Next.js',
+      'Vite.js',
       'React Query',
       'Redux',
       'Jotai',
       'Zustand',
       'Tailwind CSS',
-      'Vuejs',
+      'Vue.js',
       'Angular',
       'Supabase',
       'Firebase',
@@ -40,7 +41,17 @@ export const SKILLS = [
   { name: 'Programming Languages', list: ['HTML', 'CSS/SCSS', 'JavaScript', 'TypeScript'] }
 ]
 
-export const EXPERIENCE = [
+export const KEY_SKILLS = [
+  'Continuous Integration & Continuous Deployment',
+  'Design Thinking Process',
+  'Critical Thinking & Problem Solving',
+  'Accessible Frontend Application',
+  'Adaptability',
+  'Team Player',
+  'Creativity'
+]
+
+export const EXPERIENCE: Experience[] = [
   {
     companyName: 'Skyshi Digital Indonesia',
     role: 'Frontend Developer',
@@ -49,9 +60,10 @@ export const EXPERIENCE = [
       end: 'Present'
     },
     lists: [
-      `Build ticket purchasing web app with GlobalTix on <a href='https://baliekajaya.com/'>Baliekajaya</a>, built with Angular.`,
-      `Build ticket purchasing web app with GlobalTix on <a href='https://www.baligilitickets.com/'>Baligiliticket</a>, built with Angular.`,
-      `Developing survey based app with AcuityQuick, using React and Tailwind CSS.`
+      `Responsible to build internal and external application, mostly external client from local to international with modern web app technology with React and Next.js`,
+      `Responsible to deliver fast and optimized application, write browser backward compatibility with babel, deliver responsive-friendly and cross browser application with autoprefixer and postcss.`,
+      `Develop and maintain features from low to high impact, make sure to deliver high user experience frontend application that passed web accessibility standard.`,
+      `Write clean and maintainable code with TypeScript.`
     ]
   },
   {
@@ -62,8 +74,9 @@ export const EXPERIENCE = [
       end: 'January, 2022'
     },
     lists: [
-      `Slicing design from figma to React component for CV template on <a href='https://gethired.id/cv-online'>gethired.id</a>.`,
-      `Develop CV-Maker based app with React and Vuejs with requirement of sustainable user experience and user interface.`
+      `Responsible to develop specific features on internal project using React.js, Vue.js and Chakra UI.`,
+      `Deliver responsive-friendly and cross browser application with autoprefixer and postcss.`,
+      `Slicing design from figma to React component for CV template on <a href='https://gethired.id/cv-online'>gethired.id</a>.`
     ]
   }
 ]
@@ -93,10 +106,24 @@ export const EDUCATION: Education = [
       start: 'July, 2018',
       end: 'May, 2021'
     },
-    paragraphs: ['Graduated in May, 2021.'],
-    list: {
-      title: 'Honorable Award:',
-      listItem: ['Student Competency and Expertise Test on building web app with PHP and MySQL.']
-    }
+    paragraphs: ['Choosing Software Engineering as my main major.', 'Graduated in May, 2021.']
+  }
+]
+
+export const SUMMARY = [
+  `Frontend Developer <em>(especially on modern web technology)</em>.`,
+  'Adept at leveraging <strong>Sprint</strong> and <strong>Agile</strong> methodologies to drive process efficiencies.',
+  `Focused to frontend environment with strong Frontend core skills, but also has familiarity with other languages and tools.`,
+  'Deploy optimized apps with scalabble user interface, user experience, and user accessibility based on design thinking process.'
+]
+
+export const LANGUAGES = [
+  {
+    title: 'Indonesia',
+    level: 'Native, Fluent'
+  },
+  {
+    title: 'English',
+    level: 'Limited working proficiency'
   }
 ]
