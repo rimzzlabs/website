5ebab304dd31a6c6e4c34748ff0e506e632ecf8b
rizkimcitra <rmaulana.citra@gmail.com>
2022-10-31 18:14:39 +0700

    feat: don't use runtime caching

--- next.config.js
+++ next.config.js
@@ -1,28 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
 const isDev = process.env.NODE_ENV === 'development'
 
 const withPWA = require('next-pwa')({
-  register: true,
   dest: 'public',
-  scope: '/',
+  skipWaiting: true, // installs new SW when available without a prompt, we only need to send a reload request to user.
+  register: true,
   disable: isDev,
-  runtimeCaching,
+  // runtimeCaching,
   buildExcludes: [
     /chunks\/images\/.*$/, // Don't precache files under .next/static/chunks/images this improves next-optimized-images behaviour
     /chunks\/pages\/api\/.*/ // Dont cache the API it needs fresh serverinfo
-  ],
-  exclude: [
-    /middleware-manifest\.json$/, // exclude middleware to fix error @see https://github.com/shadowwalker/next-pwa/issues/288#issuecomment-955777098,
-    /build-manifest\.json$/,
-    /\.map$/, // dont cache map files
-    /^.*ts.*$/ // Dont let serviceworker touch the TS streams
-  ],
-  skipWaiting: true, // installs new SW when available without a prompt, we only need to send a reload request to user.
-  // dynamicStartUrl: false, // recommend: set to false if your start url always returns same HTML document, then start url will be precached, this will help to speed up first load.
-  reloadOnOnline: false, // Prevents reloads on offline/online switch
-  sourcemap: false
+  ]
+  // exclude: [
+  //   /middleware-manifest\.json$/, // exclude middleware to fix error @see https://github.com/shadowwalker/next-pwa/issues/288#issuecomment-955777098,
+  //   /build-manifest\.json$/,
+  //   /\.map$/, // dont cache map files
+  //   /^.*ts.*$/ // Dont let serviceworker touch the TS streams
+  // ],
 })
 
 /** @type {import('next').NextConfig} */
