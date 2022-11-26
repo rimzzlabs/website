f5e26ad12e27a717e2995185005c3067547ce4b9
rizkimcitra <rmaulana.citra@gmail.com>
2022-11-27 05:40:03 +0700

    style: apply prettier

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const data = {
     name: 'Rizki M Citra',
-    status: 'Alive',
+    status: 'Alive'
   }
   return res.status(200).json(data)
 }
