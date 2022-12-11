f29c86fadb496b9c09a680ac4db2cdc62ef96688
rizkimcitra <rmaulana.citra@gmail.com>
2022-12-11 20:40:53 +0700

    fix(umami): not running using next/script

--- src/components/content/blog/GiscusComment.tsx
+++ src/components/content/blog/GiscusComment.tsx
@@ -1,29 +1,18 @@
-import { isDev } from '@/libs/constants/environmentState'
-import { twclsx } from '@/libs/twclsx'
-
 import { useTheme } from '@/hooks'
 
 import Giscus from '@giscus/react'
 import { memo } from 'react'
 
 export const GiscusComment = memo(() => {
-  const { theme } = useTheme()
+  const { theme, systemTheme } = useTheme()
 
-  const loadTheme = (theme = 'preferred_color_scheme') =>
-    ({
-      dark: 'dark',
-      light: 'light',
-      preferred_color_scheme: 'preferred_color_scheme'
-    }[theme] ?? 'preferred_color_scheme')
-
-  // don't load Giscus if on development mode, you can still remove this code thought
-  // but that's would be a waste of internet data IMO
-  if (isDev) return null
+  const gcTheme = theme === 'dark' || (theme === 'system' && systemTheme === 'dark') ? 'dark' : 'light'
 
   return (
-    <div className={twclsx('mt-4 md:mt-8')}>
+    <div className='mt-4 md:mt-8'>
       <Giscus
-        theme={loadTheme(theme)}
+        lang='en'
+        theme={gcTheme}
         emitMetadata='0'
         inputPosition='top'
         repo='rizkimcitra/rizkicitra.dev'
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -17,7 +16,7 @@ export default class CustomDocument extends Document {
           <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
           {process.env.NODE_ENV === 'production' && (
-            <Script
+            <script
               async
               defer
               data-do-not-track='true'
