04a4a0fa542b58fdd7c716ecd3e39fc9a718da70
rizkimcitra <rmaulana.citra@gmail.com>
2022-12-13 00:34:50 +0700

    feat: remove partytown (issue), use next/script for umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Partytown } from '@builder.io/partytown/react'
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -10,7 +10,6 @@ export default class CustomDocument extends Document {
     return (
       <Html lang='en-US'>
         <Head>
-          <Partytown debug={true} forward={['dataLayer.push']} />
           <meta name='theme-color' content='#18181b' />
           <link rel='preload' href='/fonts/inter-var-latin.woff2' as='font' type='font/woff2' crossOrigin='true' />
           <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
@@ -18,15 +17,15 @@ export default class CustomDocument extends Document {
           <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
           {process.env.NODE_ENV === 'production' && (
-            <script
+            <Script
               async
               defer
+              strategy='afterInteractive'
               data-do-not-track='true'
               // please change to your data website id
               data-website-id='6daf05f5-92d2-430f-9cdd-1801014260da'
               // change to your hosted umami app
               src='https://umami.rizkicitra.dev/umami.js'
-              type='text/partytown'
             />
           )}
         </Head>
