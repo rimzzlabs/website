80cf04517abe799e0c0bd86021c459e16462a91c
rizkimcitra <rmaulana.citra@gmail.com>
2022-12-13 14:36:09 +0700

    feat(emptyresult.tsx): use new Image component

--- src/components/UI/common/EmptyResult.tsx
+++ src/components/UI/common/EmptyResult.tsx
@@ -1,11 +1,20 @@
-import { CustomImage } from '@/UI/images'
+import { WrappedImage } from '@/UI/images'
 
 import { twclsx } from '@/libs/twclsx'
 
 export const EmptyResult: React.FunctionComponent = () => {
   return (
     <div className={twclsx('flex flex-col items-center justify-center py-8 md:py-16')}>
-      <CustomImage src='/static/not-found.svg' display='intrinsic' width={256} height={256} alt='Not found' />
+      <WrappedImage
+        width={256}
+        height={256}
+        loading='lazy'
+        alt='Not found'
+        placeholder='blur'
+        blurDataURL='/blur.svg'
+        src='/static/not-found.svg'
+        title='An image represent no content to show'
+      />
       <p className='text-sm md:text-base mt-2.5 md:mt-4'>
         Couldn&apos;t find what you&apos;re looking for, come back later for further content!.
       </p>
