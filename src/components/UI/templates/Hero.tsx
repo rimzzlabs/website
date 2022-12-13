49d08be7f91935da2bb6de0232fd7b5280db7491
rizkimcitra <rmaulana.citra@gmail.com>
2022-12-13 14:37:12 +0700

    style(hero.tsx): adjust margin bottom

--- src/components/UI/templates/Hero.tsx
+++ src/components/UI/templates/Hero.tsx
@@ -9,7 +9,7 @@ export type HeroProps = {
 
 export const Hero: React.FunctionComponent<HeroProps> = (props) => (
   <section className={twclsx('content-auto pb-8 md:pb-16', props.className)}>
-    <h1 className={twclsx('mb-2 md:mb-4')}>{props.title}</h1>
+    <h1 className='mb-3 md:mb-4'>{props.title}</h1>
     <p>{props.description}</p>
     {props.children}
   </section>
