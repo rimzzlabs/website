ef045125928919773a356c55f2b55f0a6839a4ab
Rizki Maulana Citra <rizi.citra-guest@moladin.com>
2023-06-20 01:29:22 +0700

    refactor(home-hero-description): change descriptions

--- src/features/home/home-hero/hh-description.tsx
+++ src/features/home/home-hero/hh-description.tsx
@@ -1,25 +1,29 @@
 export const HomeHeroDescription = () => (
   <div className='mt-4 [&>p:not(:last-child)]:mb-3 [&>p]:max-w-prose md:pb-6'>
     <p>
-      I am Rizki, I am a <strong>Frontend Developer</strong>. I am also enthusiastic about frontend
-      development and enjoy working on the web. I enjoy combining my technical skills and creativity
-      to create entertaining and user-friendly websites and applications.
+      I am Rizki, and I am a <strong>frontend developer</strong>. I am passionate about{' '}
+      <strong>front-end development</strong> and enjoy working on web development. I love combining
+      my technical skills with my creativity to develop{' '}
+      <strong>fun and user-friendly websites</strong> and applications.
     </p>
     <p>
-      It&apos;s also a place where I proudly display my personal portfolio—a collection of projects
-      that demonstrate my passion and ability. Feel free to look around and see what I&apos;ve come
-      up with.
+      This is also where I showcasing my personal portfolio-a collection of a variety of projects
+      that show my passion and ability. Please feel free to see what I&apos;ve come up with.
     </p>
     <p>
-      Throughout my career, I&apos;ve developed my technical skills and kept up with the latest
-      frontend development trends and technologies. From adaptable layouts to interactive features,
-      I use the power of web technology basics to bring ideas to life and pour personality into
-      every pixel.
+      Throughout my career, I improved my technical skills and kept up with front-end development
+      trends and technologies. I utilize the power of the web&apos;s fundamentals to bring ideas to
+      life and bring detail into every pixel, from flexible layouts to interactive features.
+    </p>
+    <p>
+      I&apos;m also interested in motorcycles; I&apos;m not sure where it came from, but it&apos;s a
+      hobby of mine, and perhaps I&apos;ll share some of my posts about motorcycle-related topics
+      here.
     </p>
     <p>
       And once more, welcome to my personal website. Feel free to explore, and don&apos;t forget to
-      hit me up if you want me to improve anything that is required for this website to be more
-      pleasant for everyone to enjoy.
+      hit me up if you have any suggestions to improve anything that is required for this website to
+      be more pleasant for everyone to enjoy.
     </p>
   </div>
 )
