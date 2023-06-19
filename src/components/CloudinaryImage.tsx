0537e430376b250232399a3201f91c6e71b1d6d7
Rizki Maulana Citra <rizi.citra-guest@moladin.com>
2023-06-20 01:12:14 +0700

    style(cloudinaryimage): adjust cloudinary image url

--- src/components/CloudinaryImage.tsx
+++ src/components/CloudinaryImage.tsx
@@ -1,3 +1,4 @@
+import { CldOptions } from '@cld-apis/types'
 import buildUrl from 'cloudinary-build-url'
 import Image, { ImageProps } from 'next/image'
 import { forwardRef } from 'react'
@@ -6,23 +7,22 @@ type ImagePropsOmit = Omit<Omit<Omit<ImageProps, 'title'>, 'src'>, 'alt'>
 type Props = ImagePropsOmit & {
   title: string
   alt: string
+  publicId: string
+  buildUrlProps?: CldOptions
 }
 
-export const CloudinaryImg = forwardRef<HTMLImageElement, Props>((props, ref) => {
-  const src = buildUrl('rizkicitra.dev/post-thumbnail/improving-react-performance.jpg', {
-    cloud: {
-      cloudName: 'rizkicitra',
-    },
-    transformations: {
-      resize: {
-        type: 'scale',
-        width: 500,
-        height: 500,
+export const CloudinaryImg = forwardRef<HTMLImageElement, Props>(
+  ({ publicId, buildUrlProps, ...props }, ref) => {
+    const src = buildUrl(publicId, {
+      ...buildUrlProps,
+      cloud: {
+        ...buildUrlProps?.cloud,
+        cloudName: 'rizkicitra',
       },
-    },
-  })
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <Image src={src} ref={ref} {...props} />
-})
+    })
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <Image src={src} ref={ref} {...props} />
+  },
+)
 
 CloudinaryImg.displayName = 'CloudinaryImg'
