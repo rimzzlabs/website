5b54f4bdf8545fc1745fba95627cfda2a7320426
Rizki Maulana Citra <rizi.citra-guest@moladin.com>
2023-07-18 15:15:06 +0700

    fix: route handler, remove if condition

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,11 +2,8 @@ import { revalidatePath } from 'next/cache'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: NextRequest) {
-  const url = req.nextUrl.clone()
-  if (url.pathname.startsWith('/blog')) {
-    revalidatePath('/blog/[slug]')
-
-    return NextResponse.json({ revalidated: true, now: Date.now() })
-  }
+  revalidatePath('/blog/[slug]')
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
