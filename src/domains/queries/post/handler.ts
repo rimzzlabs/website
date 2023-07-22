039e1d5e4e34ed3651b7a424986f26a15e0795bb
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-07-23 05:51:04 +0700

    fix(post views): incorrect checking which url to use on production

--- src/domains/queries/post/handler.ts
+++ src/domains/queries/post/handler.ts
@@ -1,12 +1,19 @@
+import { SITE_URL } from '@/domains/seo'
+
 import { asyncFetchJSON } from '@/utils/async-fetch'
 
 import type { PostViewsResponseAPI } from '@/app/api/post-views/route'
 
 export const getPostViewsQuery = (slug: string) => {
   return async () => {
-    const url = `http://localhost:3222/api/post-views?slug=${slug}`
+    const baseURL = process.env.NODE_ENV === 'production' ? SITE_URL : 'http://localhost:3222'
 
-    const [data, error] = await asyncFetchJSON<PostViewsResponseAPI>(url, { method: 'GET' })
+    const url = new URL('api/post-views', baseURL)
+    url.searchParams.append('slug', slug)
+
+    const [data, error] = await asyncFetchJSON<PostViewsResponseAPI>(url.toString(), {
+      method: 'GET',
+    })
 
     if (error || !data.data) throw new Error('Cannog get post-views')
 
