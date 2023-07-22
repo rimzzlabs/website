cdbe6fa8928c3de086c00d3ff3c77e44cf997615
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-07-23 01:30:47 +0700

    chore: remove next-sitemap config

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://rizkicitra.dev',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
-  sitemapSize: 10000,
-}
