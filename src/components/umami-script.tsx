32c4991b9f7f31cae7508ed5214f9380bc9d1847
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-07-24 09:11:50 +0700

    refactor(umami script): use pattern matching to render umami script

--- src/components/umami-script.tsx
+++ src/components/umami-script.tsx
@@ -4,18 +4,25 @@ import { UMAMI_SRC, UMAMI_WEBSITE_ID } from '@/domains/umami'
 
 import Script from 'next/script'
 import { useEffect, useState } from 'react'
+import { P, match } from 'ts-pattern'
 
 export const UmamiScript = () => {
   const crawlerAgent =
-    /bot|google|aolbuild|baidu|bing|msn|duckduckgo|teoma|slurp|yandex|Lighthouse|lighthouse/i
+    /bot|google|aolbuild|baidu|bing|msn|duckduckgo|teoma|slurp|yandex|Chrome-Lighthouse/i
 
-  const [userAgent, setUserAgent] = useState('chrome')
+  const [userAgent, setUserAgent] = useState('default')
 
   const props = {
     'data-website-id': UMAMI_WEBSITE_ID,
     src: UMAMI_SRC,
   }
   const isDev = process.env.NODE_ENV === 'development'
+  const isBot = crawlerAgent.test(userAgent)
+  const matcher = {
+    isDev,
+    isBot,
+    userAgent,
+  }
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -23,7 +30,9 @@ export const UmamiScript = () => {
     }
   }, [])
 
-  if (crawlerAgent.test(userAgent) || isDev) return null
-
-  return <Script async defer id='umami-analytics' strategy='afterInteractive' {...props} />
+  return match(matcher)
+    .with({ isBot: false, isDev: false, userAgent: P.not('default') }, () => (
+      <Script async defer id='umami-analytics' strategy='afterInteractive' {...props} />
+    ))
+    .otherwise(() => null)
 }
