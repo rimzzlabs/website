9449c3fe92e80b3549febdc3bbba8187b9ca581e
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-06-02 08:25:01 +0700

    chore: remove use client directive

--- src/components/ui/tracing-beam.tsx
+++ src/components/ui/tracing-beam.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, useTransform, useScroll, useSpring } from "framer-motion";
 import { cn } from "@/lib/utils";
 
