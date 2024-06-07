a9a156953373b36fc1a9ffa55b27bbca75d58c6b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-06-07 09:53:25 +0700

    style(blog card): apply dynamic styling for post card

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,12 +23,15 @@ export default defineConfig({
   image: {
     domains: ["res.cloudinary.com"],
   },
+  output: "hybrid",
+  adapter: vercel(),
 });
-
 function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 238 });
+    const readingTime = getReadingTime(textOnPage, {
+      wordsPerMinute: 238,
+    });
     data.astro.frontmatter.readingTime = {
       text: readingTime.text,
       words: readingTime.words,
