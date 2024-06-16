dc1f2a787972b73eb86aace4cef2742fc3693913
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-06-17 03:47:25 +0700

    feat(footer): added source code url

--- src/variables/menu.ts
+++ src/variables/menu.ts
@@ -14,4 +14,10 @@ export const menu: Array<TMenu> = [
     title: "Connect",
     external: true,
   },
+  {
+    text: "Source",
+    href: "https://github.com/rimzzlabs/website",
+    title: "View this websites' source code",
+    external: true,
+  },
 ];
