339ba21b97c14f7970c423fb8c1ce7714ee839b5
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-06-17 02:01:54 +0700

    fix(blog): `Astro.site.url` return localhost

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rimzzlabs.com",
   integrations: [
     tailwind(),
     react(),
