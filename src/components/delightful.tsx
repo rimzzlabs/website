f146ca5f5276214e5de1e07d12e11f799f9ecb91
Rizki Citra <rimzzlabs@proton.me>
2025-05-19 03:17:17 +0700

    style: arjust hydration strategy for toaster

---
+++ src/components/delightful.tsx
@@ -0,0 +1,9 @@
+import { LineShadowText } from './ui/line-shadow-text'
+
+export function Delightful() {
+	return (
+		<LineShadowText shadowColor='var(--color-orange-500)' className='italic'>
+			Delightful
+		</LineShadowText>
+	)
+}
---
+++ src/modules/home/delightful.tsx
@@ -0,0 +1,5 @@
+import { LineShadowText } from '@/components/ui/line-shadow-text'
+
+export function Delightful() {
+	return <LineShadowText shadowColor='var(--color-orange-500)'>Delightful</LineShadowText>
+}
